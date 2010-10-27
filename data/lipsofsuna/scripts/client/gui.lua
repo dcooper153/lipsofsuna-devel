171b9449e6e2414c8997da74596ff9c322201d16
Ari Mustonen <amuzen@users.sourceforge.net>
2010-10-27 16:45:10 +0300

    Minor GUI improvements.

--- data/lipsofsuna/scripts/client/gui.lua
+++ data/lipsofsuna/scripts/client/gui.lua
@@ -1,68 +1,5 @@
 Gui = Class()
 
-Widgets.Scene = Class(Widget)
-
-Widgets.Scene.setter = function(self, key, value)
-	if key == "action" then
-		if self.action ~= value then
-			Widget.setter(self, key, value)
-			self:reshaped()
-		end
-	else
-		Widget.setter(self, key, value)
-	end
-end
-
-Widgets.Scene.new = function(clss, args)
-	local self = Widget.new(clss, args)
-	self.scene = Scene()
-	return self
-end
-
-Widgets.Scene.pick = function(self)
-	return Target:pick_scene()
-end
-
-Widgets.Scene.pressed = function(self)
-	Target:select_scene()
-end
-
-Widgets.Scene.render = function(self)
-	Player.camera.viewport = {self.x, self.y, self.width, self.height}
-	self.scene:draw_begin{
-		modelview = Player.camera.modelview,
-		projection = Player.camera.projection,
-		viewport = Player.camera.viewport}
-	self.scene:draw_deferred_begin()
-	self.scene:draw_deferred_opaque()
-	self.scene:draw_deferred_end()
-	self.scene:draw_forward_transparent()
-	self.scene:draw_post_process{shader = "postprocess-vert-hdr"}
-	self.scene:draw_post_process{shader = "postprocess-horz-hdr"}
-	self.scene:draw_end()
-	Speech:draw{
-		modelview = Player.camera.modelview,
-		projection = Player.camera.projection,
-		viewport = Player.camera.viewport}
-end
-
-Widgets.Scene.reshaped = function(self)
-	local w = self.width
-	local h = self.height
-	local o = 100
-	self:canvas_clear()
-	if self.action then
-		self:canvas_text{
-			dest_position = {0,h-o},
-			dest_size = {w,o},
-			text = self.action,
-			text_alignment = {0.5,0},
-			text_color = {1,1,1,1},
-			text_font = "medium"}
-	end
-	self:canvas_compile()
-end
-
 --- Initializes the in-game user interface.
 -- @param clss Gui class.
 Gui.init = function(clss)
@@ -99,7 +36,7 @@ Gui.init = function(clss)
 	Gui.menus:open{level = 1, widget = Gui.menu_widget_main}
 	-- Bottom HUD.
 	Gui.chat_history = Widgets.Log()
-	Gui.chat_entry = Widgets.Entry()
+	Gui.chat_entry = Widgets.Entry{transparent = true}
 	Gui.chat_entry.pressed = function(self)
 		Network:send{packet = Packet(packets.CHAT, "string", self.text)}
 		self:clear()
--- data/lipsofsuna/scripts/client/widgets/entry.lua
+++ data/lipsofsuna/scripts/client/widgets/entry.lua
@@ -38,16 +38,18 @@ Widgets.Entry.reshaped = function(self)
 	local w = self.width
 	local h = self.height
 	self:canvas_clear()
-	self:canvas_image{
-		dest_position = {0,0},
-		dest_size = {w,h},
-		source_image = "widgets1",
-		source_position = self.focused and {128,0} or {128,0},
-		source_tiling = {6,52,6,6,52,6}}
+	if not self.transparent then
+		self:canvas_image{
+			dest_position = {0,0},
+			dest_size = {w,h},
+			source_image = "widgets1",
+			source_position = self.focused and {128,0} or {128,0},
+			source_tiling = {6,52,6,6,52,6}}
+	end
 	self:canvas_text{
-		dest_position = {0,0},
+		dest_position = {2,0},
 		dest_size = {w,h},
-		text = self.text,
+		text = self.text and (self.text .. (self.focused and "|" or "")),
 		text_alignment = {0,0.5},
 		text_color = self.focused and {0.5,0,0,1} or {0,0,0,1},
 		text_font = self.font}
