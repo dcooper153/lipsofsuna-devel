ab8c5b0627f7691a721f698d29edc6f11fcd82b1
Ari Mustonen <amuzen@users.sourceforge.net>
2011-01-11 19:04:50 +0200

    Use a different shader for foliage.

---
+++ data/lipsofsuna/scripts/client/shaders/foliage.lua
@@ -0,0 +1,71 @@
+Shader{
+name = "foliage",
+pass1_alpha_to_coverage = true,
+pass1_color_write = false,
+pass1_depth_func = "lequal",
+pass1_vertex = [[
+out vec2 var_texcoord;
+void main()
+{
+	vec4 tmp = LOS_matrix_modelview * vec4(LOS_coord,1.0);
+	var_texcoord = LOS_texcoord;
+	gl_Position = LOS_matrix_projection * tmp;
+}]],
+pass1_fragment = [[
+in vec2 var_texcoord;
+void main()
+{
+	LOS_output_0.rgb = vec3(0.0);
+	LOS_output_0.a = texture(LOS_diffuse_texture_0, var_texcoord).a;
+}]],
+pass4_alpha_to_coverage = true,
+pass4_blend = true,
+pass4_blend_src = "one",
+pass4_blend_dst = "one",
+pass4_depth_func = "equal",
+pass4_depth_write = false,
+pass4_vertex = [[
+out fragvar
+{
+	vec3 coord;
+	vec3 lightvector;
+	vec3 normal;
+	vec3 tangent;
+	vec2 texcoord;
+} OUT;
+void main()
+{
+	vec4 tmp = LOS_matrix_modelview * vec4(LOS_coord,1.0);
+	OUT.coord = tmp.xyz;
+	OUT.lightvector = LOS_light_position_premult - tmp.xyz;
+	OUT.normal = LOS_matrix_normal * LOS_normal;
+	OUT.tangent = LOS_matrix_normal * LOS_tangent;
+	OUT.texcoord = LOS_texcoord;
+	gl_Position = LOS_matrix_projection * tmp;
+}]],
+pass4_fragment = [[
+in fragvar
+{
+	vec3 coord;
+	vec3 lightvector;
+	vec3 normal;
+	vec3 tangent;
+	vec2 texcoord;
+} IN;]]
+.. Shader.los_light_attenuation
+.. Shader.los_light_combine
+.. Shader.los_light_diffuse
+.. Shader.los_light_specular
+.. Shader.los_normal_mapping .. [[
+void main()
+{
+	vec3 tangent = normalize(IN.tangent);
+	vec3 normal = los_normal_mapping(IN.normal, tangent, IN.texcoord, LOS_diffuse_texture_1);
+	vec4 diffuse = texture(LOS_diffuse_texture_0, IN.texcoord);
+	float fattn = los_light_attenuation(IN.lightvector, LOS_light_equation);
+	float fdiff = los_light_diffuse(IN.lightvector, normal);
+	float fspec = los_light_specular(IN.coord, normal, LOS_material_shininess);
+	vec4 light = los_light_combine(fattn, fdiff, fspec, LOS_light_ambient,
+		LOS_light_diffuse, LOS_light_specular * LOS_material_specular);
+	LOS_output_0 = LOS_material_diffuse * diffuse * light;
+}]]}
