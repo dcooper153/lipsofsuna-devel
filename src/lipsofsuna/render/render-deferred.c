220a7d9ed7b6d5ae49001446a1df883472dd38c9
Ari Mustonen <amuzen@users.sourceforge.net>
2011-02-07 09:07:03 +0200

    Trying to work around weird framebuffer blit behavior on Nvidia cards. Somehow the correct mipmap level isn't blit even though the framebuffers clearly have level 0 attached to them.

--- src/lipsofsuna/render/render-deferred.c
+++ src/lipsofsuna/render/render-deferred.c
@@ -281,6 +281,7 @@ static int private_rebuild (
 		glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glGenerateMipmap (GL_TEXTURE_2D);
 	}
 	glBindTexture (GL_TEXTURE_2D, postproc_textures[2]);
 	glTexImage2D (GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT24, width, height, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, NULL);
