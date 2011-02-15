de693bdbc0cf9b3abaec676e9ce9da0514c03e87
Ari Mustonen <amuzen@users.sourceforge.net>
2011-02-15 20:32:46 +0200

    Looks like Blender changed the way how exporters are registered; fixed against the newest Blender PPA.

--- tool/lipsofsuna_export.py
+++ tool/lipsofsuna_export.py
@@ -1218,9 +1218,11 @@ def menu_func(self, context):
 	self.layout.operator(LIExporter.bl_idname, text="Lips of Suna (.lmdl)")
 
 def register():
+	bpy.utils.register_module(__name__)
 	bpy.types.INFO_MT_file_export.append(menu_func)
 
 def unregister():
+	bpy.utils.unregister_module(__name__)
 	bpy.types.INFO_MT_file_export.remove(menu_func)
 
 if __name__ == "__main__":
