17cbb25655fa138f09f61eccc410e0031935de31
Ari Mustonen <amuzen@users.sourceforge.net>
2011-02-16 00:09:29 +0200

    Reduced geometry shader load in the fur shader by doing lighting in the vertex shader.

--- data/lipsofsuna/scripts/client/shaders/fur.lua
+++ data/lipsofsuna/scripts/client/shaders/fur.lua
@@ -20,6 +20,7 @@ pass6_vertex = [[
 out geovar
 {
 	vec3 coord;
+	vec4 light;
 	vec3 normal;
 	vec3 tangent;
 	vec2 texcoord;
@@ -30,6 +31,10 @@ void main()
 	OUT.coord = tmp.xyz;
 	OUT.normal = LOS_matrix_normal * LOS_normal;
 	OUT.texcoord = LOS_texcoord;
+	vec3 lightvector[LOS_LIGHT_MAX];
+	]] .. Shader.los_lighting_vectors("lightvector", "tmp.xyz") .. [[
+	]] .. Shader.los_lighting_default("tmp.xyz", "OUT.normal", "lightvector") .. [[
+	OUT.light = lighting;
 	gl_Position = LOS_matrix_projection * tmp;
 }]],
 pass6_geometry = [[
@@ -38,6 +43,7 @@ layout(triangle_strip, max_vertices=30) out;
 in geovar
 {
 	vec3 coord;
+	vec4 light;
 	vec3 normal;
 	vec3 tangent;
 	vec2 texcoord;
@@ -46,8 +52,7 @@ out fragvar
 {
 	float layer;
 	vec4 color;
-	vec3 coord;
-	vec3 lightvector[LOS_LIGHT_MAX];
+	vec4 light;
 	vec3 normal;
 	vec2 texcoord;
 	vec2 texcoord2;
@@ -63,12 +68,12 @@ void main()
 		for(i = 0 ; i < gl_VerticesIn ; i++)
 		{
 			OUT.color = color;
-			OUT.coord = IN[i].coord + IN[i].normal * OUT.layer * 0.03;
-			]] .. Shader.los_lighting_vectors("OUT.lightvector", "OUT.coord") .. [[
-			OUT.normal = IN[i].normal + 0.2 * vec3(sin(OUT.layer), cos(OUT.layer), cos(OUT.layer-1.8));
+			OUT.light = IN[i].light;
+			OUT.normal = IN[i].normal;
 			OUT.texcoord = IN[i].texcoord;
 			OUT.texcoord2 = IN[i].texcoord + 0.1 * vec2(sin(OUT.layer), cos(OUT.layer));
-			gl_Position = LOS_matrix_projection * vec4(OUT.coord, 1.0);
+			vec3 coord = IN[i].coord + IN[i].normal * OUT.layer * 0.03;
+			gl_Position = LOS_matrix_projection * vec4(coord, 1.0);
 			EmitVertex();
 		}
 		EndPrimitive();
@@ -79,8 +84,7 @@ in fragvar
 {
 	float layer;
 	vec4 color;
-	vec3 coord;
-	vec3 lightvector[LOS_LIGHT_MAX];
+	vec4 light;
 	vec3 normal;
 	vec2 texcoord;
 	vec2 texcoord2;
@@ -90,9 +94,8 @@ void main()
 	vec3 normal = normalize(IN.normal);
 	vec4 diffuse = texture(LOS_diffuse_texture_0, IN.texcoord);
 	vec4 mask = texture(LOS_diffuse_texture_1, IN.texcoord2);
-	]] .. Shader.los_lighting_default("IN.coord", "normal", "IN.lightvector") .. [[
 	if(IN.layer <= 0.0001)
-		LOS_output_0 = LOS_material_diffuse * diffuse * lighting;
+		LOS_output_0 = LOS_material_diffuse * diffuse * IN.light;
 	else
-		LOS_output_0 = LOS_material_diffuse * diffuse * IN.color * mask * lighting;
+		LOS_output_0 = LOS_material_diffuse * diffuse * IN.color * mask * IN.light;
 }]]}
