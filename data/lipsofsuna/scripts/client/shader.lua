2fa2abf60480fa41b661eda8df8ab79f2de6b2ee
Ari Mustonen <amuzen@users.sourceforge.net>
2011-03-09 08:31:40 +0200

    Removed useless shader helpers.

--- data/lipsofsuna/scripts/client/shader.lua
+++ data/lipsofsuna/scripts/client/shader.lua
@@ -62,27 +62,6 @@ Shader.set_quality = function(self, value)
 	end
 end
 
-Shader.los_light_attenuation = [[
-float los_light_attenuation(in vec3 coord, in vec3 equation)
-{
-	float d = length(coord);
-	return 1.0 / (equation.x + equation.y * d + equation.z * d * d);
-}]]
-
-Shader.los_light_combine = [[
-vec4 los_light_combine(in float fattn, in float fdiff, in float fspec, in vec4 cambi, in vec4 cdiff, in vec4 cspec)
-{
-	return vec4(fattn * (cambi.rgb + fdiff * cdiff.rgb + fspec * cspec.rgb), 1.0);
-}]]
-
-Shader.los_light_diffuse = [[
-float los_light_diffuse(in vec3 coord, in vec3 normal)
-{
-	vec3 dir = normalize(coord);
-	float coeff = dot(normal, dir);
-	return max(0.0, coeff);
-}]]
-
 Shader.los_lighting_default = function(co, no, lv, sp)
 	return string.format([[int lighting_index;
 	vec4 lighting = vec4(0.0, 0.0, 0.0, 1.0);
@@ -124,41 +103,12 @@ Shader.los_lighting_vectors = function(lv, co)
 		]] .. lv .. [[[lighting_vindex] = LOS_light[lighting_vindex].position_premult - ]] .. co .. [[;]]
 end
 
-Shader.los_light_specular = [[
-float los_light_specular(in vec3 coord, in vec3 normal, in float shininess)
-{
-	vec3 refl = reflect(-normalize(coord), normal);
-	float coeff = max(0.0, dot(normal, refl));
-	return pow(coeff, shininess);
-}]]
-
-Shader.los_light_spot = [[
-float los_light_spot(in vec3 coord, in vec3 spotdir, in vec3 spotcfg)
-{
-	/* spotcfg = (cutoff, coscutoff, exponent) */
-	float coeff = clamp(dot(spotdir, -normalize(coord)), -1.0, 1.0);
-	float cutoff = step(spotcfg.y, 0.0);
-	float blend = step(spotcfg.y, coeff);
-	return blend * max(cutoff, pow(coeff, spotcfg.z));
-}]]
-
 Shader.los_normal_mapping = [[
 vec3 los_normal_mapping(in vec3 normal, in vec3 tangent, in vec2 texcoord, in sampler2D sampler)
 {
 	vec3 tmp = normalize(normal);
 	mat3 tangentspace = mat3(tangent, tmp * tangent, tmp);
 	vec3 n = normalize(texture(sampler, texcoord).xyz * 2.0 - 1.0);
-	return normalize(tangentspace * n);
-}]]
-
-Shader.normalmapping = [[
-vec3 normalmapping()
-{
-	vec3 nor = normalize(var_normal);
-	vec3 tan = normalize(var_tangent);
-	vec3 bin = nor * tan;
-	mat3 tangentspace = mat3(tan, bin, nor);
-	vec3 n = normalize(texture2D(uni_texturemap[1], var_texcoord).xyz * 2.0 - 1.0);
 	return normalize(tangentspace * n);
 }]]
 
