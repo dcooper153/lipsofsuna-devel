1ebfc96568aac33f2e485d6a26bb6bbf73aa78ed
Ari Mustonen <amuzen@users.sourceforge.net>
2011-03-23 09:34:30 +0200

    Added leggings graphics for android.

--- data/lipsofsuna/scripts/content/items.lua
+++ data/lipsofsuna/scripts/content/items.lua
@@ -855,7 +855,8 @@ Itemspec{
 	categories = {"armor"},
 	crafting_materials = {hide = 4},
 	equipment_models = {
-		["aer/kraken/wyrm"] = {feet = false, legs = "aerleggings1"}},
+		["aer/kraken/wyrm"] = {feet = false, legs = "aerleggings1"},
+		["android"] = {feet = false, legs = "androidleggings1"}},
 	equipment_slot = "feet",
 	mass = 5,
 	water_gravity = Vector(0,1,0)}
