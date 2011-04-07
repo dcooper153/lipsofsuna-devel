cf68bf350186d251ec90e850b428c3d5ec7bc8cf
Ari Mustonen <amuzen@users.sourceforge.net>
2011-04-07 15:52:01 +0300

    Fixed a bug in the admin tool.

--- data/lipsofsuna/scripts/server/editing.lua
+++ data/lipsofsuna/scripts/server/editing.lua
@@ -1,9 +1,9 @@
-Protocol:add_handler{type = "EDIT_TILE", func = function(event)
+Protocol:add_handler{type = "EDIT_TILE", func = function(args)
 	-- Check for permissions.
 	local player = Player:find{client = args.client}
 	if not player.admin then player:send("You have no permission to do that.") end
 	-- Set tile.
-	local ok,x,y,z,t = event.packet:read("uint32", "uint32", "uint32", "uint8")
+	local ok,x,y,z,t = args.packet:read("uint32", "uint32", "uint32", "uint8")
 	if ok then
 		Voxel:set_tile(Vector(x, y, z), t)
 	end
