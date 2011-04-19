d529adaf35ee7b42f0607134230597b7418808ee
Ari Mustonen <amuzen@users.sourceforge.net>
2011-04-19 22:17:14 +0300

    Made returning to the login screen possible if login failed.

--- data/lipsofsuna/scripts/client/views/startup.lua
+++ data/lipsofsuna/scripts/client/views/startup.lua
@@ -7,15 +7,22 @@ Startup.init = function(clss)
 	clss.group:set_expand{col = 1, row = 1}
 	clss.group:set_expand{col = 3}
 	clss.text = Widgets.Label()
-	clss.button_retry = Widgets.Label{font = "mainmenu", text = "Retry", pressed = function() clss:retry() end}
+	clss.button_back = Widgets.Label{font = "mainmenu", text = "Back", pressed = function() clss:back() end}
 	clss.button_quit = Widgets.Label{font = "mainmenu", text = "Quit", pressed = function() Program:shutdown() end}
 	clss.group2 = Widget{cols = 4, rows = 1, margins = {bottom=30}, spacings = {horz=40}}
-	clss.group2:set_child{col = 2, row = 1, widget = clss.button_retry}
+	clss.group2:set_child{col = 2, row = 1, widget = clss.button_back}
 	clss.group2:set_child{col = 3, row = 1, widget = clss.button_quit}
 	clss.group2:set_expand{col = 1}
 	clss.group2:set_expand{col = 4}
 	clss.group:set_child{col = 2, row = 2, widget = clss.text}
 	clss.group:set_child{col = 2, row = 3, widget = clss.group2}
+end
+
+--- Returns to the launcher.
+-- @param clss Startup class.
+Startup.back = function(clss)
+	Program:launch_mod{name = "default"}
+	Program.quit = true
 end
 
 --- Finishes the startup when connection has been established.
@@ -43,13 +50,13 @@ Startup.retry = function(clss)
 		-- Host a game.
 		Program:unload_world()
 		Client:host("--file " .. Settings.file .. " --server " .. Settings.address .. " " .. Settings.port)
-		clss:set_state("Starting the server on port " .. Settings.port .. "...", "Retry")
+		clss:set_state("Starting the server on port " .. Settings.port .. "...")
 		clss.host_wait_timer = Timer{delay = 2, func = function(timer)
 			if Network:join{host = "localhost", Settings.port} then
-				clss:set_state("Connecting to the server...", "Retry")
+				clss:set_state("Connecting to the server...")
 				clss.connecting = true
 			else
-				clss:set_state("Failed to start the server!", "Retry")
+				clss:set_state("Failed to start the server!")
 				clss.connecting = nil
 			end
 			timer:disable()
@@ -58,10 +65,10 @@ Startup.retry = function(clss)
 		-- Join a game.
 		Program:unload_world()
 		if Network:join{host = Settings.address, port = Settings.port} then
-			clss:set_state("Connecting to " .. Settings.address .. ":" .. Settings.port .. "...", "Retry")
+			clss:set_state("Connecting to " .. Settings.address .. ":" .. Settings.port .. "...")
 			clss.connecting = true
 		else
-			clss:set_state("Failed to connect to " .. Settings.address .. ":" .. Settings.port .. "!", "Retry")
+			clss:set_state("Failed to connect to " .. Settings.address .. ":" .. Settings.port .. "!")
 			clss.connecting = nil
 		end
 	end
@@ -70,14 +77,7 @@ end
 --- Shows or updates the waiting screen.
 -- @param clss Startup class.
 -- @param text Waiting text.
--- @param button Retry button text or nil for no button.
-Startup.set_state = function(clss, text, button)
-	if button then
-		clss.button_retry.text = button
-		clss.button_retry.visible = true
-	else
-		clss.button_retry.visible = false
-	end
+Startup.set_state = function(clss, text)
 	clss.text.text = text
 end
 
@@ -86,7 +86,7 @@ Startup:init()
 Eventhandler{type = "tick", func = function(self, args)
 	if Startup.joined and not Network.connected then
 		Gui:set_mode("startup")
-		Startup:set_state("Lost connection to the server!", "Reconnect")
+		Startup:set_state("Lost connection to the server!")
 	end
 end}
 
@@ -94,7 +94,7 @@ Protocol:add_handler{type = "AUTHENTICATE_REJECT", func = function(event)
 	local ok,s = event.packet:read("string")
 	if not ok then return end
 	Gui:set_mode("startup")
-	Startup:set_state("Authentication failed: " .. s, "Retry")
+	Startup:set_state("Authentication failed: " .. s)
 end}
 
 Protocol:add_handler{type = "CLIENT_AUTHENTICATE", func = function(event)
