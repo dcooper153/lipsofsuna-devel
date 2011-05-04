684132a775f84672701801464112c85e3920a7bc
Ari Mustonen <amuzen@users.sourceforge.net>
2011-05-04 19:10:40 +0300

    Improved editor.sh to automatically create patterns.

--- tool/editor.sh
+++ tool/editor.sh
@@ -5,6 +5,14 @@ echo $(ls ./data/lipsofsuna/scripts/content/patterns)
 
 NUM=1
 
+if [ -e ./data/lipsofsuna/scripts/content/patterns/$@.lua ]; then 
+  echo "file found, opening"
+  else
+  echo "Not an existing pattern, making a new one"
+  echo "Pattern{ \n	name = \"$@\" ,\n	size = Vector(32,8,32)}" > ./data/lipsofsuna/scripts/content/patterns/$@.lua
+  echo "require \"content/patterns/$@\"" >> ./data/lipsofsuna/scripts/content/patterns.lua
+fi
+
 while [ -e ./data/lipsofsuna/scripts/content/patterns/$@mod$NUM.lua ] ; do
 
   echo " $NUM file exists, incrementing "
