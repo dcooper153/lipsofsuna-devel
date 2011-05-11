2869304722096648bfd28b3ac71ca960b80358c0
Ari Mustonen <amuzen@users.sourceforge.net>
2011-05-11 19:12:53 +0300

    Allowed scripts to add and remove vertices of models.

--- data/system/object-render.lua
+++ data/system/object-render.lua
@@ -7,13 +7,31 @@ end
 
 ------------------------------------------------------------------------------
 
+--- Adds a material to the model.
+-- @param self Object.
+-- @param ... Arguments.<ul>
+--   <li>cull: False to disable face culling.</li>
+--   <li>shader: Shader name.</li></ul>
+Model.add_material = function(self, ...)
+	Los.model_add_material(self.handle, ...)
+end
+
+--- Adds triangles to the model.
+-- @param self Object.
+-- @param ... Arguments.<ul>
+--   <li>material: Material index.</li>
+--   <li>vertices: Array of vertices.</li></ul>
+Model.add_triangles = function(self, ...)
+	Los.model_add_triangles(self.handle, ...)
+end
+
 --- Edits the matching material.
 -- @param self Object.
 -- @param args Arguments.<ul>
 --   <li>diffuse: Diffuse color to set or nil.</li>
 --   <li>match_shader: Shader name to match or nil.</li>
 --   <li>match_texture: Texture name to match or nil.</li>
---   <li>specular: Specular color to set or nil.</li>
+--   <li>specular: Specular color to set or nil.</li></ul>
 Model.edit_material = function(self, args)
 	Los.model_edit_material(self.handle, args)
 end
@@ -23,7 +41,7 @@ end
 -- @param ... Arguments.<ul>
 --   <li>1,shape: Shape key name.</li>
 --   <li>2,value: Shape influence multiplier.</li>
---   <li>3,ref: Reference model for relative morphing, or nil.</ul>
+--   <li>3,ref: Reference model for relative morphing, or nil.</li></ul>
 Model.morph = function(self, ...)
 	local a,b,c = ...
 	if type(a) == "table" then
@@ -31,6 +49,12 @@ Model.morph = function(self, ...)
 	else
 		Los.model_morph(self.handle, a, b, c and c.handle)
 	end
+end
+
+--- Removes the vertices and indices of the model.
+-- @param self Object.
+Model.remove_vertices = function(self)
+	Los.model_remove_vertices(self.handle)
 end
 
 ------------------------------------------------------------------------------
--- src/lipsofsuna/extension/object-render/ext-model.c
+++ src/lipsofsuna/extension/object-render/ext-model.c
@@ -24,6 +24,168 @@
 
 #include "ext-module.h"
 
+static void Model_add_material (LIScrArgs* args)
+{
+	int cull;
+	int index;
+	const char* shader;
+	LIMdlBuilder* builder;
+	LIMdlMaterial material;
+	LIExtModule* module;
+	LIEngModel* model;
+
+	/* Get the engine model. */
+	module = liscr_script_get_userdata (args->script, LIEXT_SCRIPT_RENDER_MODEL);
+	model = args->self;
+
+	/* Create a model builder. */
+	builder = limdl_builder_new (model->model);
+	if (builder == NULL)
+		return;
+
+	/* Read material properties. */
+	/* TODO: Support more. */
+	limdl_material_init (&material);
+	if (liscr_args_gets_bool (args, "cull", &cull) && !cull)
+		material.flags &= ~LIMDL_MATERIAL_FLAG_CULLFACE;
+	if (liscr_args_gets_string (args, "shader", &shader))
+		limdl_material_set_shader (&material, shader);
+
+	/* Insert the material. */
+	index = model->model->materials.count;
+	if (limdl_builder_insert_material (builder, &material))
+		limdl_builder_insert_facegroup (builder, index);
+	limdl_builder_finish (builder);
+
+	/* Cleanup. */
+	limdl_builder_free (builder);
+	limdl_material_free (&material);
+}
+
+static void Model_add_triangles (LIScrArgs* args)
+{
+	int i;
+	int group = 1;
+	int vertices_cap = 0;
+	int vertices_num = 0;
+	float* attrs[8];
+	uint32_t* indices = NULL;
+	LIMatVector zero = { 0.0f, 0.0f, 0.0f };
+	LIMdlBuilder* builder;
+	LIMdlVertex* tmp;
+	LIMdlVertex* vertex;
+	LIMdlVertex* vertices = NULL;
+	LIExtModule* module;
+	LIEngModel* model;
+
+	/* Get the engine model. */
+	module = liscr_script_get_userdata (args->script, LIEXT_SCRIPT_RENDER_MODEL);
+	model = args->self;
+
+	/* Get the edited material group. */
+	liscr_args_gets_int (args, "material", &group);
+	if (group < 1 || group > model->model->materials.count)
+		return;
+
+	/* Read vertex data. */
+	/* Vertices are stored to a sequential table. Each vertex is presented as
+	   a sequential child table whose fields are presented in the order of
+	   {x,y,z,nx,ny,nz,u,v}. */
+	if (liscr_args_gets_table (args, "vertices"))
+	{
+		for (vertices_num = 0 ; ; vertices_num++)
+		{
+			/* Get the vertex table. */
+			lua_pushnumber (args->lua, vertices_num + 1);
+			lua_gettable (args->lua, -2);
+			if (lua_type (args->lua, -1) != LUA_TTABLE)
+			{
+				lua_pop (args->lua, 1);
+				break;
+			}
+
+			/* Allocate space for the vertex. */
+			if (vertices_cap < vertices_num + 1)
+			{
+				if (!vertices_cap)
+					vertices_cap = 32;
+				else
+					vertices_cap <<= 1;
+				tmp = realloc (vertices, vertices_cap * sizeof (LIMdlVertex));
+				if (tmp == NULL)
+				{
+					lisys_free (vertices);
+					return;
+				}
+				vertices = tmp;
+			}
+			vertex = vertices + vertices_num;
+			limdl_vertex_init (vertex, &zero, &zero, 0.0f, 0.0f);
+
+			/* Read vertex attributes. */
+			attrs[0] = &vertex->coord.x;
+			attrs[1] = &vertex->coord.y;
+			attrs[2] = &vertex->coord.z;
+			attrs[3] = &vertex->normal.x;
+			attrs[4] = &vertex->normal.y;
+			attrs[5] = &vertex->normal.z;
+			attrs[6] = vertex->texcoord + 0;
+			attrs[7] = vertex->texcoord + 1;
+			for (i = 0 ; i < 8 ; i++)
+			{
+				lua_pushnumber (args->lua, i + 1);
+				lua_gettable (args->lua, -2);
+				if (lua_type (args->lua, -1) == LUA_TNUMBER)
+					*(attrs[i]) = lua_tonumber (args->lua, -1);
+				else
+					*(attrs[i]) = 0.0f;
+				lua_pop (args->lua, 1);
+			}
+			lua_pop (args->lua, 1);
+		}
+		lua_pop (args->lua, 1);
+	}
+
+	/* Validate the vertex count. */
+	if (!vertices_num)
+		return;
+	vertices_num -= vertices_num % 3;
+	if (!vertices_num)
+	{
+		lisys_free (vertices);
+		return;
+	}
+
+	/* Create the index buffer. */
+	indices = calloc (vertices_num, sizeof (uint32_t));
+	if (indices == NULL)
+	{
+		lisys_free (vertices);
+		return;
+	}
+	for (i = 0 ; i < vertices_num ; i++)
+		indices[i] = model->model->vertices.count + i;
+
+	/* Create a model builder. */
+	builder = limdl_builder_new (model->model);
+	if (builder == NULL)
+	{
+		lisys_free (vertices);
+		lisys_free (indices);
+		return;
+	}
+
+	/* Insert the vertices and indices. */
+	limdl_builder_insert_vertices (builder, vertices, vertices_num, NULL);
+	limdl_builder_insert_indices (builder, group - 1, indices, vertices_num);
+	limdl_builder_finish (builder);
+
+	/* Cleanup. */
+	limdl_builder_free (builder);
+	lisys_free (vertices);
+	lisys_free (indices);
+}
+
 static void Model_edit_material (LIScrArgs* args)
 {
 	int i;
@@ -76,13 +238,36 @@ static void Model_morph (LIScrArgs* args)
 		(ref != NULL)? ref->model : NULL, shape, value));
 }
 
+static void Model_remove_vertices (LIScrArgs* args)
+{
+	int i;
+	LIEngModel* model;
+	LIMdlFaces* group;
+
+	model = args->self;
+	for (i = 0 ; i < model->model->facegroups.count ; i++)
+	{
+		group = model->model->facegroups.array + i;
+		lisys_free (group->indices.array);
+		group->indices.array = NULL;
+		group->indices.capacity = 0;
+		group->indices.count = 0;
+	}
+	lisys_free (model->model->vertices.array);
+	model->model->vertices.array = NULL;
+	model->model->vertices.count = 0;
+}
+
 /*****************************************************************************/
 
 void liext_script_render_model (
 	LIScrScript* self)
 {
+	liscr_script_insert_mfunc (self, LISCR_SCRIPT_MODEL, "model_add_material", Model_add_material);
+	liscr_script_insert_mfunc (self, LISCR_SCRIPT_MODEL, "model_add_triangles", Model_add_triangles);
 	liscr_script_insert_mfunc (self, LISCR_SCRIPT_MODEL, "model_edit_material", Model_edit_material);
 	liscr_script_insert_mfunc (self, LISCR_SCRIPT_MODEL, "model_morph", Model_morph);
+	liscr_script_insert_mfunc (self, LISCR_SCRIPT_MODEL, "model_remove_vertices", Model_remove_vertices);
 }
 
 /** @} */
