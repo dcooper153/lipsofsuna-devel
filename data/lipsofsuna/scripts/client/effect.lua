dcooper153/lipsofsuna-devel

commit c5579ba9f993f1e193538f0318fd6cbcd3b18d76

Fixed the rotation of the beheading particle effect.

data/lipsofsuna/scripts/client/effect.lua
@@ -1,13 +1,48 @@
 Effect.play = function(clss, name)
-	local e = Effect:find{name = name}
-	if not e then return end
+	if not Player.object then return end
+	clss:play_object(Player.object, name)
+end
+
+Effect.play_object = function(clss, name, object, node)
+	-- Find the effect.
+	local effect = Effect:find{name = name}
+	if not effect then return end
+	-- Find the node.
+	local p
+	if node then
+		p = object:find_node{name = node}
+		if not p then return end
+	end
+	-- Create the effect object.
 	EffectObject{
-		object = Player.object,
-		sound = e.sound,
-		sound_delay = e.sound_delay,
-		sound_pitch = e.sound_pitch,
-		sound_positional = e.sound_positional,
+		model = effect.model,
+		object = object,
+		node = node,
+		rotation_inherit = (effect.rotation ~= false),
+		sound = effect.sound,
+		sound_delay = effect.sound_delay,
+		sound_pitch = effect.sound_pitch,
+		sound_positional = effect.sound_positional,
 		realized = true}
+	-- Quake the camera.
+	Player:apply_quake(object.position, effect.quake)
+end
+
+Effect.play_world = function(clss, name, position)
+	-- Find the effect.
+	local effect = Effect:find{name = name}
+	if not effect then return end
+	-- Create the effect object.
+	EffectObject{
+		model = effect.model,
+		position = Vector(x,y,z),
+		sound = effect.sound,
+		sound_delay = effect.sound_delay,
+		sound_pitch = effect.sound_pitch,
+		sound_positional = effect.sound_positional,
+		realized = true}
+	-- Quake the camera.
+	Player:apply_quake(Vector(x,y,z), effect.quake)
 end
 
 EffectObject = Class(Object)
@@ -22,6 +57,7 @@ EffectObject = Class(Object)
 --   <li>node: Parent node or nil.</li>
 --   <li>object: Parent object or nil.</li>
 --   <li>position: Position in world space.</li>
+--   <li>rotation_inherit: True to inherit rotation from the parent object.</li>
 --   <li>sound: Sound effect name.</li>
 --   <li>sound_delay: Sound delay in seconds.</li>
 --   <li>sound_pitch: Sound effect pitch range.</li>
@@ -34,6 +70,7 @@ EffectObject.new = function(clss, args)
 	local parent = args.object
 	local node = args.node
 	local velocity = args.velocity or Vector()
+	local rotate = args.rotation_inherit
 	-- Attach a model effect.
 	local self = Object.new(clss, args)
 	if Object.particle_animation then
@@ -71,10 +108,10 @@ EffectObject.new = function(clss, args)
 		local p = node and parent:find_node{name = node}
 		if p then
 			self.position = parent.position + parent.rotation * p
-			self.rotation = parent.rotation
+			if rotate then self.rotation = parent.rotation end
 		else
 			self.position = parent.position + self.position
-			self.rotation = parent.rotation
+			if rotate then self.rotation = parent.rotation end
 		end
 	end
 	-- Update in a thread until the effect ends.
@@ -88,10 +125,10 @@ EffectObject.new = function(clss, args)
 				local p = node and parent:find_node{name = node}
 				if p then
 					self.position = parent.position + parent.rotation * p
-					self.rotation = parent.rotation
+					if rotate then self.rotation = parent.rotation end
 				else
 					self.position = parent.position + moved
-					self.rotation = parent.rotation
+					if rotate then self.rotation = parent.rotation end
 				end
 			elseif args.velocity then
 				self.position = moved

data/lipsofsuna/scripts/common/effect.lua
@@ -15,6 +15,7 @@ end
 -- @param args Arguments.<ul>
 --   <li>model: Model to show.</li>
 --   <li>name: Name of the effect.</li>
+--   <li>rotation: False to not inherit rotation from the parent object.</li>
 --   <li>sound: Sound effect to play.</li>
 --   <li>sound_delay: Delay of the sound start, in seconds.</li>
 --   <li>sound_pitch: Pitch range of the sound effect.</li>

data/lipsofsuna/scripts/client/client.lua
@@ -39,19 +39,6 @@ end}
 Protocol:add_handler{type = "WORLD_EFFECT", func = function(event)
 	local ok,t,x,y,z = event.packet:read("string", "float", "float", "float")
 	if ok then
-		-- Find the effect.
-		local effect = Effect:find{name = t}
-		if not effect then return end
-		-- Create the effect object.
-		EffectObject{
-			model = effect.model,
-			position = Vector(x,y,z),
-			sound = effect.sound,
-			sound_delay = effect.sound_delay,
-			sound_pitch = effect.sound_pitch,
-			sound_positional = effect.sound_positional,
-			realized = true}
-		-- Quake the camera.
-		Player:apply_quake(Vector(x,y,z), effect.quake)
+		Effect:play_world(t, Vector(x,y,z))
 	end
 end}

data/lipsofsuna/scripts/client/network/object.lua
@@ -15,19 +15,7 @@ Protocol:add_handler{type = "OBJECT_BEHEADED", func = function(event)
 	o.flags = Bitwise:bor(o.flags, Protocol.object_flags.BEHEADED)
 	o:update_model()
 	-- Play a particle effect.
-	local p = o:find_node{name = "#neck"}
-	local effect = Effect:find{name = "behead1"}
-	if p and effect then
-		EffectObject{
-			model = effect.model,
-			object = o,
-			node = "#neck",
-			sound = effect.sound,
-			sound_delay = effect.sound_delay,
-			sound_pitch = effect.sound_pitch,
-			sound_positional = effect.sound_positional,
-			realized = true}
-	end
+	Effect:play_object("behead1", o, "#neck")
 end}
 
 Protocol:add_handler{type = "OBJECT_DAMAGE", func = function(args)
@@ -82,20 +70,8 @@ Protocol:add_handler{type = "OBJECT_EFFECT", func = function(event)
 		-- Find the object.
 		local obj = Object:find{id = i}
 		if not obj then return end
-		-- Find the effect.
-		local effect = Effect:find{name = e}
-		if not effect then return end
-		-- Create an effect object.
-		EffectObject{
-			model = effect.model,
-			object = obj,
-			sound = effect.sound,
-			sound_delay = effect.sound_delay,
-			sound_pitch = effect.sound_pitch,
-			sound_positional = effect.sound_positional,
-			realized = true}
-		-- Quake the camera.
-		Player:apply_quake(obj.position, effect.quake)
+		-- Play the effect.
+		Effect:play_object(e, obj)
 	end
 end}
 

data/lipsofsuna/scripts/content/effects.lua
@@ -5,6 +5,7 @@ Effect{
 Effect{
 	name = "behead1",
 	model = "blood2",
+	rotation = false,
 	sound = "squish-000"}
 
 Effect{

commit 18fda24cff655a3de00d1b4086d894fa41eb1770

Fixed sound effect playback breakage introduced in c5579ba9.

data/lipsofsuna/scripts/client/effect.lua
@@ -35,7 +35,7 @@ Effect.play_world = function(clss, name, position)
 	-- Create the effect object.
 	EffectObject{
 		model = effect.model,
-		position = Vector(x,y,z),
+		position = position,
 		sound = effect.sound,
 		sound_delay = effect.sound_delay,
 		sound_pitch = effect.sound_pitch,