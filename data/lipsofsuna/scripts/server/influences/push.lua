09a6406ff0cbff3c407093b1d50a94a231625d1e
Ryan Swart <theancientgoat@users.sourceforge.net>
2011-06-23 19:58:59 +0200

    added more item descriptions, changed push/mindless march spell a bit

--- data/lipsofsuna/scripts/server/influences/push.lua
+++ data/lipsofsuna/scripts/server/influences/push.lua
@@ -1,4 +1,5 @@
 Influencespec{name = "push", func = function(feat, info, args, value)
 	if not args.target then return end
+	args.target:face_point{point = args.attacker.position, 0}
 	args.target:set_movement(value)
 end} 
