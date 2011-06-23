c0aa13848fefc6a2f7731cde751f6b30e6bef42e
Ari Mustonen <amuzen@users.sourceforge.net>
2011-06-24 01:39:22 +0300

    Added new melee system animations for aer, android and kraken.

--- data/lipsofsuna/scripts/server/feat.lua
+++ data/lipsofsuna/scripts/server/feat.lua
@@ -133,6 +133,15 @@ Feat.perform = function(self, args)
 	local slot = anim and anim.slot
 	local weapon = slot and args.user:get_item{slot = slot}
 	local info = anim and self:get_info{attacker = args.user, weapon = weapon}
+	-- Helper called by individual feat animations.
+	local play_effects = function(self)
+		if anim.effect then
+			Effect:play{effect = anim.effect, object = args.user}
+		end
+		if weapon and weapon.spec.effect_attack then
+			Effect:play{effect = weapon.spec.effect_attack, object = args.user}
+		end
+	end
 	-- Check for cooldown and requirements.
 	if info and not args.stop then
 		if args.user.cooldown then return end
@@ -151,17 +160,8 @@ Feat.perform = function(self, args)
 	if args.user.attack_charge then
 		charge = Program.time - args.user.attack_charge
 	end
-	-- Animate the feat.
-	Vision:event{type = "object-feat", object = args.user, anim = anim}
-	local play_effects = function(self)
-		if anim.effect then
-			Effect:play{effect = anim.effect, object = args.user}
-		end
-		if weapon and weapon.spec.effect_attack then
-			Effect:play{effect = weapon.spec.effect_attack, object = args.user}
-		end
-	end
 	-- Call the feat function.
+	local move
 	if not info or anim.toggle or not args.stop then
 		if anim then
 			if anim.categories["build"] then
@@ -185,8 +185,21 @@ Feat.perform = function(self, args)
 			end
 			if anim.categories["melee"] then
 				-- Melee attack.
-				-- While the attack animation is played, an attack ray is cast.
-				-- The first object or tile that collides with the ray is damaged.
+				-- While the attack animation is played, the blade is moved along a path.
+				-- The first object or tile that collides with the blade ray is damaged.
+				-- The shape of the blade path depends on the controls of the user.
+				local paths = {
+					--[[Stand]] {Vector(0.1, 0, 0.75), Vector(-0.1, 0, 0.5), Vector(0.05, 0, 0.25), Vector(-0.05, 0, 0)},
+					--[[Left]] {Vector(0.5, 0.1, 1), Vector(0.25, 0.05, 0.7), Vector(0, 0, 0.5), Vector(-0.25, 0, 0.7)},
+					--[[Right]] {Vector(0.5, 0.1, 0.7), Vector(0.1, 0.05, 0.5), Vector(-0.2, 0, 0.6), Vector(-0.5, 0, 1)},
+					--[[Back]] {Vector(0.1, 0.2, 1), Vector(0.05, 0.2, 0.75), Vector(0, 0.1, 0.5), Vector(0, -0.1, 0.25)},
+					--[[Forward]] {Vector(0.3, -0.1, 0.8), Vector(0.2, -0.1, 0.55), Vector(0.1, -0.05, 0.3), Vector(0, 0.05, 0.15)}}
+				if args.user.strafing < -0.2 then move = 2
+				elseif args.user.strafing > 0.2 then move = 3
+				elseif args.user.movement < -0.2 then move = 4
+				elseif args.user.movement > 0.2 then move = 5
+				else move = 1 end
+				local path = paths[move]
 				Thread(function(t)
 					local apply = function(r)
 						self:apply{
@@ -210,18 +223,6 @@ Feat.perform = function(self, args)
 						if r then return apply(r) end
 						prev = dst
 					end
-					-- Choose the blade path.
-					local path_n = {Vector(0.1, 0, 0.75), Vector(-0.1, 0, 0.5), Vector(0.05, 0, 0.25), Vector(-0.05, 0, 0)}
-					local path_l = {Vector(0.5, 0.1, 1), Vector(0.25, 0.05, 0.7), Vector(0, 0, 0.5), Vector(-0.25, 0, 0.7)}
-					local path_r = {Vector(0.5, 0.1, 0.7), Vector(0.1, 0.05, 0.5), Vector(-0.2, 0, 0.6), Vector(-0.5, 0, 1)}
-					local path_b = {Vector(0.1, 0.2, 1), Vector(0.05, 0.2, 0.75), Vector(0, 0.1, 0.5), Vector(0, -0.1, 0.25)}
-					local path_f = {Vector(0.3, -0.1, 0.8), Vector(0.2, -0.1, 0.55), Vector(0.1, -0.05, 0.3), Vector(0, 0.05, 0.15)}
-					local path
-					if args.user.strafing < -0.2 then path = path_l
-					elseif args.user.strafing > 0.2 then path = path_r
-					elseif args.user.movement < -0.2 then path = path_b
-					elseif args.user.movement > 0.2 then path = path_f
-					else path = path_n end
 					-- Cast attack rays.
 					play_effects()
 					for i = 1,4 do
@@ -337,6 +338,9 @@ Feat.perform = function(self, args)
 		end
 		if self.func then self:func(args) end
 	end
+	-- Animate the feat.
+	-- The move parameter is used by melee feats to tell which attack animation was used.
+	Vision:event{type = "object-feat", object = args.user, anim = anim, move = move}
 	return true
 end
 
