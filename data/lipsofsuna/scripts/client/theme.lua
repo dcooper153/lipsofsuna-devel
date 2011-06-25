4baa384ff1c61b3cd0ef6df61a18e734ec6d0d28
Ari Mustonen <amuzen@users.sourceforge.net>
2011-06-25 13:13:07 +0300

    Reduced the size of the mainmenu font.

--- data/lipsofsuna/scripts/client/theme.lua
+++ data/lipsofsuna/scripts/client/theme.lua
@@ -1,5 +1,5 @@
 Widgets:add_font_style("default", "serif", 14)
 Widgets:add_font_style("bigger", "serif", 18)
 Widgets:add_font_style("medium", "serif", 24)
-Widgets:add_font_style("mainmenu", "serif", 64)
+Widgets:add_font_style("mainmenu", "serif", 48)
 Widgets:add_font_style("tiny", "serif", 9)
