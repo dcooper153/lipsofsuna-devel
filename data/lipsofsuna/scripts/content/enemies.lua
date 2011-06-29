d3906720f1c94708c6d102064b4842e509326eb2
Francisco Athens <omniscientcolossus@yahoo.com>
2011-06-29 13:39:48 -0700

    bitingbark script

--- data/lipsofsuna/scripts/content/enemies.lua
+++ data/lipsofsuna/scripts/content/enemies.lua
@@ -170,6 +170,7 @@ Species{
 		{name = "health", max = 30, val = 30},
 		{name = "strength", max = 5, val = 5}}}
 
+
 Species{
 	name = "dragon",
 	base = "base",
@@ -400,3 +401,29 @@ Species{
 		{name = "health", max = 20, val = 20},
 		{name = "strength", max = 10, val = 10}},
 	vulnerabilities = {cold = 2, fire = 0.4, physical = 0.8}}
+
+Species{
+	name = "bitingbark",
+	base = "base",
+	ai_enable_strafe = false,
+	ai_enable_block = false,
+	ai_enable_jump = false,
+	ai_enable_weapon_switch = false,
+	ai_offense_factor = 1,
+	animations = {
+		["attack left claw"] = {animation = "claw-left", channel = 2, weight = 30},
+		["attack right claw"] = {animation = "claw-right", channel = 2, weight = 30}},
+	categories = {"enemy"},
+	difficulty = 0,
+	effect_physical_damage = "fleshimpact1",
+	factions = {"evil"},
+	feat_anims = {"left claw", "right claw"},
+	feat_effects = {"strength to physical damage"},
+	inventory_size = 3,
+	inventory_items = {"log", "log"},
+	jump_force = 0,
+	model = "bitingbark1",
+	speed_walk = 1,
+	skills = {
+		{name = "health", max = 30, val = 30},
+		{name = "strength", max = 5, val = 5}}}
