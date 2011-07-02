af7a92314912c4c2b0cd09310572bea9cb14a197
Ari Mustonen <amuzen@users.sourceforge.net>
2011-07-02 18:22:36 +0300

    Fixed an AMD shader problem probably caused by an oversized vertex shader.

--- data/lipsofsuna/scripts/client/shaders/skin.lua
+++ data/lipsofsuna/scripts/client/shaders/skin.lua
@@ -23,7 +23,7 @@ void main()
 }]]},
 
 -- Medium quality program.
--- Vertex lighting, fake skin diffuse.
+-- Fragment lighting, fake skin diffuse.
 medium = {
 pass1_color_write = false,
 pass1_depth_func = "lequal",
@@ -41,21 +41,19 @@ void main()
 pass4_depth_func = "equal",
 pass4_depth_write = false,
 pass4_vertex = [[
+out vec3 F_coord;
+out vec3 F_halfvector[LOS_LIGHT_MAX];
+out vec3 F_lightvector[LOS_LIGHT_MAX];
 out vec3 F_normal;
 out vec2 F_texcoord;
-out vec4 F_light;
 void main()
 {
 	]] .. Shader.los_animation_default() .. [[
 	vec4 tmp = LOS_matrix_modelview * vec4(anim_coord,1.0);
-	vec3 coord = tmp.xyz;
+	]] .. Shader.los_lighting_vectors("F_lightvector", "F_halfvector", "tmp.xyz") .. [[
+	F_coord = tmp.xyz;
 	F_normal = LOS_matrix_normal * anim_normal;
 	F_texcoord = LOS_texcoord;
-	vec3 halfvector[LOS_LIGHT_MAX];
-	vec3 lightvector[LOS_LIGHT_MAX];
-	]] .. Shader.los_lighting_vectors("lightvector", "halfvector", "tmp.xyz") .. [[
-	]] .. Shader.los_lighting_skin("coord", "F_normal", "lightvector", "halfvector") .. [[
-	F_light = lighting;
 	gl_Position = LOS_matrix_projection * tmp;
 }]],
 pass4_fragment = [[
@@ -66,9 +64,12 @@ void main()
 {
 	vec3 normal = normalize(F_normal);
 	vec4 diffuse = texture(LOS_diffuse_texture_0, F_texcoord);
-	float fake = abs(dot(normal, vec3(1.0,0.0,0.0))) + abs(dot(normal, vec3(0.0,1.0,0.0)));
-	diffuse = mix(LOS_material_diffuse * diffuse, vec4(0.5,0.0,0.0,1.0), 0.3 * fake - 0.15);
-	LOS_output_0 = diffuse * F_light;
+	]] .. Shader.los_lighting_skin("F_coord", "normal", "F_lightvector", "F_halfvector") .. [[
+	float fake1 = 0.5 * abs(dot(normal, vec3(1.0,0.0,0.0))) + abs(dot(normal, vec3(0.0,1.0,0.0)));
+	float fake2 = 1.0 - abs(dot(normal, vec3(0.0,0.0,1.0)));
+	float fake = mix(fake1, fake2, 0.7);
+	diffuse = LOS_material_diffuse * mix(diffuse, vec4(1.0,0.0,0.0,1.0), 0.3 * fake);
+	LOS_output_0 = diffuse * lighting;
 }]]},
 
 -- High quality program.
