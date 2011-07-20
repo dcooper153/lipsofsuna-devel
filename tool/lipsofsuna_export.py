eea26624917a8e083e0e5440937979d07a154427
Ari Mustonen <amuzen@users.sourceforge.net>
2011-07-20 19:28:30 +0300

    Integrated shape key mirroring to the exporter.

--- tool/lipsofsuna_export.py
+++ tool/lipsofsuna_export.py
@@ -12,6 +12,7 @@ bl_info = {
 	"category": "Import-Export"}
 
 import array, math, os, re, struct, sys, bpy, mathutils
+from bpy_extras.object_utils import object_data_add
 
 class LIEnum:
 	def __init__(self):
@@ -68,6 +69,9 @@ class LIExportFinishedDialog(bpy.types.Operator):
 			col5 = flow.column()
 			col5.scale_x = 0.15
 			col5.label(text = "NOD")
+			col6 = flow.column()
+			col6.scale_x = 0.15
+			col6.label(text = "KEY")
 			for file in LIFormat.files:
 				info = file.get_finish_info()
 				col1.label(text = file.filename)
@@ -75,6 +79,7 @@ class LIExportFinishedDialog(bpy.types.Operator):
 				col3.label(text = "%s" % info[1])
 				col4.label(text = "%s" % info[2])
 				col5.label(text = "%s" % info[3])
+				col6.label(text = "%s" % info[4])
 			layout.column()
 		else:
 			col = layout.column()
@@ -168,6 +173,172 @@ def object_check_export(object, file, type='NODE'):
 		return True
 	else:
 		return False
+
+# \brief Mirrors the object and its shape keys.
+# \param obj Object to mirror.
+# \param mod Mirror modifier.
+def apply_shape_key_mirror(obj, mod):
+	mirror_g = mod.use_mirror_vertex_groups
+	mirror_u = mod.use_mirror_u
+	mirror_v = mod.use_mirror_v
+	mirror_x = mod.use_x
+	faces = []
+	face_newtoold = {}
+	face_oldtonew = {}
+	verts = []
+	vert_merged = {}
+	vert_newtoold = {}
+	vert_oldtonew = {}
+	# Store original counts.
+	num_verts = len(obj.data.vertices)
+	num_edges = len(obj.data.edges)
+	num_faces = len(obj.data.faces)
+	num_groups = len(obj.vertex_groups)
+	num_shapes = len(obj.data.shape_keys.key_blocks)
+	num_layers = 0
+	if obj.data.uv_textures:
+		num_layers = len(obj.data.uv_textures)
+	# Add original vertices.
+	for i in range(0,num_verts):
+		verts.append(obj.data.vertices[i].co)
+		vert_newtoold[i] = i
+		vert_oldtonew[i] = i
+	# Add mirrored vertices.
+	for i in range(0,num_verts):
+		vert = verts[i]
+		if vert.x > -0.001 and vert.x < 0.001:
+			vert_newtoold[i] = i
+			vert_oldtonew[i] = i
+			vert_merged[i] = True
+		else:
+			l = len(verts)
+			vert_newtoold[l] = i
+			vert_oldtonew[i] = l
+			verts.append(mathutils.Vector((-vert.x, vert.y, vert.z)))
+	# Add original faces.
+	for i in range(0,num_faces):
+		face = obj.data.faces[i]
+		if len(face.vertices) == 3:
+			indices = [face.vertices[0], face.vertices[1], face.vertices[2]]
+		else:
+			indices = [face.vertices[0], face.vertices[1], face.vertices[2], face.vertices[3]]
+		face_newtoold[i] = i
+		face_oldtonew[i] = i
+		faces.append(indices)
+	# Add mirrored faces.
+	for i in range(0,num_faces):
+		face = faces[i]
+		if len(face) == 3:
+			indices = [face[2], face[1], face[0]]
+		else:
+			indices = [face[3], face[2], face[1], face[0]]
+		for j in range(0,len(face)):
+			indices[j] = vert_oldtonew[indices[j]]
+		l = len(faces)
+		face_newtoold[l] = i
+		face_oldtonew[i] = l
+		faces.append(indices)
+	# Create the mesh.
+	mesh = bpy.data.meshes.new("LosTmp")
+	mesh.from_pydata(verts, [], faces)
+	mesh.update()
+	# Add complete vertex data.
+	for i in range(0,len(verts)):
+		old_vert = obj.data.vertices[vert_newtoold[i]]
+		new_vert = mesh.vertices[i]
+		if i in vert_merged:
+			n = old_vert.normal.copy()
+			n.x = 0
+			new_vert.normal = n.normalized()
+		else:
+			new_vert.normal = old_vert.normal.copy()
+	# Create materials.
+	for mat in obj.data.materials:
+		mesh.materials.append(mat)
+	# Create texture layers.
+	for i in range(0,num_layers):
+		mesh.uv_textures.new(obj.data.uv_textures[i].name)
+	# Add complete face data.
+	for i in range(0,len(faces)):
+		old_face = obj.data.faces[face_newtoold[i]]
+		new_face = mesh.faces[i]
+		new_face.hide = old_face.hide
+		new_face.material_index = old_face.material_index
+		new_face.select = old_face.select
+		new_face.use_smooth = old_face.use_smooth
+		for j in range(0,num_layers):
+			old_uv = obj.data.uv_textures[j].data[face_newtoold[i]]
+			new_uv = mesh.uv_textures[j].data[i]
+			new_uv.blend_type = old_uv.blend_type
+			new_uv.hide = old_uv.hide
+			new_uv.image = old_uv.image
+			new_uv.pin_uv = old_uv.pin_uv
+			new_uv.select_uv = old_uv.select_uv
+			new_uv.use_alpha_sort = old_uv.use_alpha_sort
+			new_uv.use_billboard = old_uv.use_billboard
+			new_uv.use_bitmap_text = old_uv.use_bitmap_text
+			new_uv.use_blend_shared = old_uv.use_blend_shared
+			new_uv.use_collision = old_uv.use_collision
+			new_uv.use_halo = old_uv.use_halo
+			new_uv.use_image = old_uv.use_image
+			new_uv.use_light = old_uv.use_light
+			new_uv.use_object_color = old_uv.use_object_color
+			new_uv.use_shadow_cast = old_uv.use_shadow_cast
+			new_uv.use_twoside = old_uv.use_twoside
+			if not mirror_u or face_newtoold[i] == i:
+				new_uv.uv = [(uv[0], uv[1]) for uv in old_uv.uv]
+			else:
+				uv = [(1.0-uv[0], uv[1]) for uv in old_uv.uv]
+				if len(uv) == 3:
+					new_uv.uv = [uv[2], uv[1], uv[0]]
+				else:
+					new_uv.uv = [uv[3], uv[2], uv[1], uv[0]]
+	# Create the object.
+	object_data_add(bpy.context, mesh, operator=None)
+	new_obj = bpy.context.scene.objects.active
+	# Create shape keys.
+	for i in range(0,num_shapes):
+		old_shape = obj.data.shape_keys.key_blocks[i]
+		new_obj.shape_key_add(name=old_shape.name, from_mix=False)
+		new_shape = new_obj.data.shape_keys.key_blocks[i]
+		for j in range(0,len(verts)):
+			old_vert = old_shape.data[vert_newtoold[j]]
+			new_vert = new_shape.data[j]
+			new_vert.co = old_vert.co.copy()
+			if j != vert_newtoold[j]:
+				new_vert.co.x *= -1.0
+	# Create vertex groups.
+	for i in range(0,num_groups):
+		old_group = obj.vertex_groups[i]
+		new_group = new_obj.vertex_groups.new(name=old_group.name)
+	for i in range(0,len(verts)):
+		old_vert = obj.data.vertices[vert_newtoold[i]]
+		new_vert = mesh.vertices[i]
+		for j in range(0,len(old_vert.groups)):
+			old_elem = old_vert.groups[j]
+			old_group = obj.vertex_groups[old_elem.group]
+			if mirror_g and i != vert_newtoold[i]:
+				new_group_name = re.sub(r'\.L$', '.R', old_group.name)
+				if new_group_name == old_group.name:
+					new_group_name = re.sub(r'\.R$', '.L', old_group.name)
+				if new_group_name in new_obj.vertex_groups:
+					new_group = new_obj.vertex_groups[new_group_name]
+				else:
+					new_group = new_obj.vertex_groups[old_group.name]
+			else:
+				new_group = new_obj.vertex_groups[old_elem.group]
+			new_group.add([i], old_elem.weight, 'REPLACE')
+	# Copy custom properties.
+	def copyprop(prop):
+		try:
+			v = obj[prop]
+			new_obj[prop] = v
+		except:
+			pass
+	copyprop("export")
+	copyprop("file")
+	copyprop("render")
+	copyprop("shape")
 
 ##############################################################################
 
@@ -1221,6 +1392,7 @@ class LIVertex:
 class LIFile:
 
 	def __init__(self, file):
+		self.junkobjs = []
 		self.tempobjs = []
 		self.origobjs = {}
 		self.coll = LICollision()
@@ -1239,13 +1411,15 @@ class LIFile:
 			self.filepath = os.path.join(path1, file)
 
 	def get_finish_info(self):
-		info = [0, 0, 0, 0]
+		info = [0, 0, 0, 0, 0]
 		if self.mesh:
 			info[0] = len(self.mesh.vertlist)
 			info[1] = len(self.mesh.matlist)
 		if self.hier:
 			info[2] = len(self.hier.animlist)
 			info[3] = len(self.hier.nodelist)
+		if self.mesh:
+			info[4] = len(self.mesh.shapekeylist)
 		return info
 
 	def get_progress_info(self):
@@ -1305,6 +1479,13 @@ class LIFile:
 				for mod in obj.modifiers:
 					if mod.type == 'ARMATURE' or mod.type == 'MULTIRES':
 						obj.modifiers.remove(mod)
+					elif mod.type == "MIRROR" and obj.data.shape_keys:
+						apply_shape_key_mirror(obj, mod)
+						self.junkobjs.append(obj)
+						obj.select = False
+						newobj = bpy.context.scene.objects.active
+						newobj.select = True
+						self.tempobjs[self.tempobjs.index(obj)] = newobj
 					else:
 						num += 1
 				# Apply modifiers.
@@ -1362,7 +1543,14 @@ class LIFile:
 			return False
 		# Delete the temporary mesh.
 		elif self.state == 9:
+			need = False
 			if self.object != None:
+				bpy.ops.object.delete()
+				need = True
+			for obj in self.junkobjs:
+				obj.select = True
+				need = True
+			if need:
 				bpy.ops.object.delete()
 			self.state += 1
 			return False
