cfec2a849cec9268bac5c81e9d52e665e7634cd2
Ari Mustonen <amuzen@users.sourceforge.net>
2011-07-27 18:20:29 +0300

    Fixed broken menu positioning.

--- data/lipsofsuna/scripts/client/widgets/menus.lua
+++ data/lipsofsuna/scripts/client/widgets/menus.lua
@@ -7,6 +7,7 @@ Widgets.Menus.new = function(clss, args)
 end
 
 Widgets.Menus.back = function(self)
+	Gui.need_rebuild = true
 	local w = self.stack[#self.stack]
 	if w.back then
 		w:back()
@@ -21,6 +22,7 @@ end
 --   <li>level: Activated menu level.</li>
 --   <li>widget: Submenu to show.</li></ul>
 Widgets.Menus.open = function(self, args)
+	Gui.need_rebuild = true
 	-- Close levels if opening from an upper level.
 	if #self.stack >= args.level then
 		self:close{hide = false, level = args.level}
@@ -39,6 +41,7 @@ end
 --   <li>hide: False to not hide the menu even if it becomes empty.</li>
 --   <li>level: Closed menu level or nil to close all.</li></ul>
 Widgets.Menus.close = function(self, args)
+	Gui.need_rebuild = true
 	local h = not args or args.hide ~= false
 	-- Pop levels from the stack.
 	local l = math.max(1, args and args.level or 1)
