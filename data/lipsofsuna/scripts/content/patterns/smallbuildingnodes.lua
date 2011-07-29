24b7a61ee490f5eacd80d130f4593f7dc6f4862e
Ryan Swart <theancientgoat@users.sourceforge.net>
2011-07-29 20:33:43 +0200

    Added another, crucial, city building pattern that I forgot to add

---
+++ data/lipsofsuna/scripts/content/patterns/smallbuildingnodes.lua
@@ -0,0 +1,10 @@
+Pattern{
+	name = "smallbuildingnodes",
+	size = Vector(10,10,10),
+	categories = {},
+obstacles = {
+{1,02,2,"small building marker"},{6,02,2,"small building marker"},
+{1,02,7,"small building marker"},{6,02,7,"small building marker"}},
+	
+	tiles = {
+		{0,0,0,"brick1"}}}
