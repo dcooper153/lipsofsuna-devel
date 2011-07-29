a9ec66991ba4ca37f1201071542d3ceabf7f09b7
Ryan Swart <theancientgoat@users.sourceforge.net>
2011-07-29 16:59:22 +0200

    Added a couple of files I forgot to commit for the castle

---
+++ data/lipsofsuna/scripts/content/patterns/swallns.lua
@@ -0,0 +1,23 @@
+Pattern{
+	name = "swallns",
+	size = Vector(5,5,5),
+	categories = {},
+	tiles = {
+		{2,0,0,"brick1"}, {3,0,0,"brick1"}, {4,0,0,"brick1"},
+		{2,1,0,"brick1"}, {3,1,0,"brick1"}, {4,1,0,"brick1"},
+		{2,2,0,"brick1"}, {3,2,0,"brick1"}, {4,2,0,"brick1"},
+		{2,3,0,"brick1"}, {4,3,0,"brick1"},
+		{2,0,1,"brick1"}, {3,0,1,"brick1"}, {4,0,1,"brick1"},
+		{2,1,1,"brick1"}, {3,1,1,"brick1"}, {4,1,1,"brick1"},
+		{2,2,1,"brick1"}, {3,2,1,"brick1"}, {4,2,1,"brick1"},
+		{2,0,2,"brick1"}, {3,0,2,"brick1"}, {4,0,2,"brick1"},
+		{2,1,2,"brick1"}, {3,1,2,"brick1"}, {4,1,2,"brick1"},
+		{2,2,2,"brick1"}, {3,2,2,"brick1"}, {4,2,2,"brick1"},
+		{2,3,2,"brick1"}, {4,3,2,"brick1"},
+		{2,0,3,"brick1"}, {3,0,3,"brick1"}, {4,0,3,"brick1"},
+		{2,1,3,"brick1"}, {3,1,3,"brick1"}, {4,1,3,"brick1"},
+		{2,2,3,"brick1"}, {3,2,3,"brick1"}, {4,2,3,"brick1"},
+		{2,0,4,"brick1"}, {3,0,4,"brick1"}, {4,0,4,"brick1"},
+		{2,1,4,"brick1"}, {3,1,4,"brick1"}, {4,1,4,"brick1"},
+		{2,2,4,"brick1"}, {3,2,4,"brick1"}, {4,2,4,"brick1"},
+		{2,3,4,"brick1"}, {4,3,4,"brick1"}}}
