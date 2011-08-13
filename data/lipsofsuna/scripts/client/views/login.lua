45d5f6420e7e76a626ddcdcd6c95cdc7363efdf9
Ryan Swart <theancientgoat@users.sourceforge.net>
2011-08-13 17:32:04 +0200

    Implemented a rather dirty quickplay method to help the poor confused people who try joining first before hosting

--- data/lipsofsuna/scripts/client/views/login.lua
+++ data/lipsofsuna/scripts/client/views/login.lua
@@ -9,25 +9,30 @@ Views.Login.new = function(clss)
 	self.text = Widgets.Label()
 	-- Menu labels.
 	self.button_intro = Widgets.Label{font = "mainmenu", text = "Intro", pressed = function() Client:set_mode("intro") end}
+	self.button_play = Widgets.Label{font = "mainmenu", text = "Play Now!", pressed = function() self:play() end}
 	self.button_host = Widgets.Label{font = "mainmenu", text = "Host", pressed = function() Client:set_mode("host") end}
 	self.button_join = Widgets.Label{font = "mainmenu", text = "Join", pressed = function() Client:set_mode("join") end}
 	self.button_controls = Widgets.Label{font = "mainmenu", text = "Controls", pressed = function() Client:set_mode("controls") end}
 	self.button_options = Widgets.Label{font = "mainmenu", text = "Options", pressed = function() Client:set_mode("options") end}
 	self.button_editor = Widgets.Label{font = "mainmenu", text = "Editor", pressed = function() Client:set_mode("editor") end}
 	self.button_quit = Widgets.Label{font = "mainmenu", text = "Quit", pressed = function() Program:shutdown() end}
+	self.toprow = Widget{cols = 1, rows = 2}
 	self.group_buttons = Widget{cols = 9, rows = 1, margins = {bottom = 30}, spacings = {horz = 30}}
 	self.group_buttons:set_expand{col = 1}
 	self.group_buttons:set_expand{col = 9}
 	--self.group_buttons:set_child{col = 2, row = 1, widget = self.button_intro}
+	self.toprow:set_child{col = 1, row = 1, widget = self.button_play}
 	self.group_buttons:set_child{col = 3, row = 1, widget = self.button_host}
 	self.group_buttons:set_child{col = 4, row = 1, widget = self.button_join}
 	self.group_buttons:set_child{col = 5, row = 1, widget = self.button_editor}
 	self.group_buttons:set_child{col = 6, row = 1, widget = self.button_controls}
 	self.group_buttons:set_child{col = 7, row = 1, widget = self.button_options}
 	self.group_buttons:set_child{col = 8, row = 1, widget = self.button_quit}
+	self.toprow:set_child{col=1,row=2,widget= self.group_buttons}
 	-- Main.
 	self:set_expand{col = 1, row = 1}
-	self:set_child(1, 2, self.group_buttons)
+	
+	self:set_child(1, 2, self.toprow)
 	self:set_child(1, 1, self.label_hint)
 	return self
 end
@@ -72,4 +77,34 @@ tab = swap quickbar mode
 esc = game menu
 You can reconfigure controls in the game menu
 ]]
+end
+
+--- Initiates hosting of a game.
+-- @param self Host view.
+Views.Login.play = function(self)
+	-- Save config.
+	local saveslot = 1
+	local account = "guest"
+	local password = ""
+	Client.config.host_saveslot = saveslot
+	Client.config.host_account = account
+	Client.config:save()
+	-- Setup hosting.
+	Settings.address = "localhost"
+	Settings.file = saveslot
+	Settings.admin = false
+	Settings.generate = false
+	Settings.host = true
+	Settings.port = 10101
+	if account and #account > 0 then
+		Settings.account = account
+	else
+		Settings.account = "guest"
+	end
+	if password and #password > 0 then
+		Settings.password = password
+	else
+		Settings.password = ""
+	end
+	Client:set_mode("startup")
 end
