dcooper153/lipsofsuna-devel

commit ecf97db9a07833785fd6b8c8a30f3d918fc4811d

Mouse motion updates scrollbars and skill widgets when LMB is pressed.

data/system/widgets/widgets.lua
@@ -8,7 +8,7 @@ end
 
 Widgets.handle_event = function(clss, args)
 	if Program.cursor_grabbed then return end
-	local generic = {keypress = true, keyrelease = true, mousemotion = true, mouserelease = true}
+	local generic = {keypress = true, keyrelease = true, mouserelease = true}
 	if args.type == "mousepress" then
 		-- Buttons.
 		local w = clss:find_handler_widget("pressed")
@@ -19,6 +19,11 @@ Widgets.handle_event = function(clss, args)
 		local w = clss:find_handler_widget("scrolled")
 		if clss:handle_popups(w) then return end
 		if w then return w:scrolled(args) end
+	elseif args.type == "mousemotion" then
+		-- Motion.
+		local w = clss:find_handler_widget("mousemotion")
+		if clss:handle_popups(w) then return end
+		if w then return w:mousemotion(args) end
 	elseif generic[args.type] then
 		-- Other events.
 		local w = clss:find_handler_widget("event")

data/lipsofsuna/client/views/chargen.lua
@@ -324,6 +324,14 @@ Views.Chargen.enter = function(self, from)
 	Sound:switch_music_track("char")
 end
 
+Views.Chargen.mousemotion = function(self, args)
+	if self.dragging then
+		local y = self.view_offset.y + args.dy / 300
+		self.view_offset.y = math.min(math.max(y, 1), 2)
+		self:rotate(math.pi * args.dx / 300)
+	end
+end
+
 Views.Chargen.pressed = function(self, args)
 	if args.button == 1 then
 		self.dragging = true
@@ -334,12 +342,6 @@ Views.Chargen.event = function(self, args)
 	if args.type == "mouserelease" then
 		if args.button == 1 then
 			self.dragging = nil
-		end
-	elseif args.type == "mousemotion" then
-		if self.dragging then
-			local y = self.view_offset.y + args.dy / 300
-			self.view_offset.y = math.min(math.max(y, 1), 2)
-			self:rotate(math.pi * args.dx / 300)
 		end
 	end
 end

data/lipsofsuna/client/widgets/skillcontrol.lua
@@ -37,6 +37,14 @@ Widgets.SkillControl.get_value_at = function(self, point)
 	end
 end
 
+Widgets.SkillControl.mousemotion = function(self, args)
+	if math.mod(Program.mouse_button_state, 2) == 1 then
+		local v = self:get_value_at(Vector(args.x, args.y))
+		if not v then return end
+		self:changed(v)
+	end
+end
+
 Widgets.SkillControl.pressed = function(self)
 	local v = self:get_value_at(Program.cursor_position)
 	if not v then return end

data/system/widgets/scrollbar.lua
@@ -18,6 +18,28 @@ Widgets.Scrollbar.get_value_at = function(self, point)
 	return math.floor(v + 0.5)
 end
 
+Widgets.Scrollbar.set_value_at = function(self, point)
+	-- Ignore if the scroll range is empty.
+	if self.max <= self.page then return end
+	-- Clamp clicks on the buttons.
+	local c = point - Vector(self.x, self.y)
+	c.y = math.max(c.y, 21)
+	c.y = math.min(c.y, self.height - 21 - 1)
+	-- Scroll to the cursor.
+	local v = self:get_value_at(point)
+	if not v then return end
+	self.scroll_offset = math.max(0, math.min(v - math.floor(self.page/2), self.max - self.page))
+	self:reshaped()
+	self:changed(self.scroll_offset)
+	return true
+end
+
+Widgets.Scrollbar.mousemotion = function(self, args)
+	if math.mod(Program.mouse_button_state, 2) == 1 then
+		self:set_value_at(Vector(args.x, args.y))
+	end
+end
+
 Widgets.Scrollbar.pressed = function(self)
 	if self.max <= self.page then return end
 	local cursor = Program.cursor_position
@@ -37,12 +59,8 @@ Widgets.Scrollbar.pressed = function(self)
 			self:changed(self.scroll_offset)
 		end
 	else
-		-- Scroll to cursor.
-		local v = self:get_value_at(cursor)
-		if not v then return end
-		self.scroll_offset = math.max(0, math.min(v - math.floor(self.page/2), self.max - self.page))
-		self:reshaped()
-		self:changed(self.scroll_offset)
+		-- Scroll to the cursor.
+		self:set_value_at(cursor)
 	end
 end
 

commit 12b75a9fb7afa52dc197bce97ca8ad1b634cfc3e

Fixed combo box widget breakage caused by commit ecf97db9.

data/system/widgets/widgets.lua
@@ -22,7 +22,6 @@ Widgets.handle_event = function(clss, args)
 	elseif args.type == "mousemotion" then
 		-- Motion.
 		local w = clss:find_handler_widget("mousemotion")
-		if clss:handle_popups(w) then return end
 		if w then return w:mousemotion(args) end
 	elseif generic[args.type] then
 		-- Other events.