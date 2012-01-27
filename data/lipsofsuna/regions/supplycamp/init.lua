0dce0c2327aa6c4af33e24bfc07ea2f323b2f762
Ari Mustonen <amuzen@users.sourceforge.net>
2012-01-27 08:02:32 +0200

    Added a dummy supply camp region.

---
+++ data/lipsofsuna/regions/supplycamp/init.lua
@@ -0,0 +1,21 @@
+Program:add_path(Mod.path)
+
+Regionspec{
+	name = "Supply Camp",
+	categories = {"special"},
+	depth = {1300,1300},
+	pattern_name = "supplycamp1",
+	position = {940,1050},
+	spawn_point = {5,5,5}}
+
+Obstaclespec{
+	name = "entrance to suna dungeons",
+	model = "portal1",
+	categories = {"portal", "special"},
+	dialog = "entrance to suna dungeons",
+	marker = "entrance to suna dungeons"}
+
+Dialogspec{name = "entrance to suna dungeons",
+	{"teleport", marker = "lyra"}}
+
+require(Mod.path .. "supplycamp1")
