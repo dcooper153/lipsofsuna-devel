9fc738a6c78de211f497722991319bb40c6e1ac1
Ari Mustonen <amuzen@users.sourceforge.net>
2012-04-17 08:03:50 +0300

    Fixed random plants not being correctly aligned to the heightmap.

--- data/lipsofsuna/server/generator/overworld.lua
+++ data/lipsofsuna/server/generator/overworld.lua
@@ -8,11 +8,6 @@ Generator.Overworld.class_name = "Generator.Overworld"
 -- @param pos Offset of the generated area.
 -- @param size Size of the generated area.
 Generator.Overworld.generate = function(self, pos, size)
-	local create_plant_or_item = function(ctr)
-		-- FIXME: The Y offset is often incorrect.
-		ctr = ctr + Vector (0.5, 0, 0.5)
-		Voxel:place_obstacle{point = ctr, category = "small-plant"}
-	end
 	-- Generate objects.
 	local c = Vector()
 	for i=1,25 do
@@ -21,7 +16,7 @@ Generator.Overworld.generate = function(self, pos, size)
 		c.z = pos.z + math.random(1, size.z - 2)
 		local p = Utils:find_spawn_point(c * Voxel.tile_size)
 		if p then
-			create_plant_or_item(p * Voxel.tile_scale)
+			Voxel:place_obstacle{point = p * Voxel.tile_scale, category = "small-plant"}
 		end
 	end
 end
