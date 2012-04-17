cad686eca406a1e890b5adfa763d83d97fe3d82a
Ari Mustonen <amuzen@users.sourceforge.net>
2012-04-17 07:59:45 +0300

    Moved the quest spec class to core/specs/.

--- data/lipsofsuna/core/init.lua
+++ data/lipsofsuna/core/init.lua
@@ -7,7 +7,6 @@ if not Settings then
 	require "common/effect"
 	require "common/model"
 	require "common/material"
-	require "common/quest"
 else
 	Voxel.blocks_per_line = 1
 	Voxel.tiles_per_line = 12
@@ -20,7 +19,6 @@ else
 	require "common/names"
 	require "common/object"
 	require "common/protocol"
-	require "common/quest"
 	require "common/serialize"
 	require "common/sectors"
 	require "common/voxel"
