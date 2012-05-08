e153c8da708c73d29ec8c807c0e64933244f1568
Ari Mustonen <amuzen@users.sourceforge.net>
2012-05-08 08:16:23 +0300

    Fixed quests attached to books not starting anymore.

--- data/lipsofsuna/server/actions/read.lua
+++ data/lipsofsuna/server/actions/read.lua
@@ -1,6 +1,15 @@
 Actionspec:extend{
 	name = "read",
 	func = function(item, user)
+		-- Trigger book dialogs.
+		if not item.dialog then
+			local dialog = Dialog{object = item, user = user}
+			if dialog then
+				item.dialog = dialog
+				item.dialog:execute()
+			end
+		end
+		-- Send the book text.
 		user:send{packet = Packet(packets.BOOK,
 			"string", item.spec.name,
 			"string", item.spec.book_text)}
