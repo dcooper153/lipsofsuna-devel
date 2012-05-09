d4c944870aaa82dcbe7e5a19d29f40c140e10047
Ari Mustonen <amuzen@users.sourceforge.net>
2012-05-09 14:04:53 +0300

    Added random voxel rock above ground.

--- data/lipsofsuna/server/generator/overworld.lua
+++ data/lipsofsuna/server/generator/overworld.lua
@@ -2,12 +2,35 @@ require "server/generator"
 
 Generator.Overworld = Class()
 Generator.Overworld.class_name = "Generator.Overworld"
+Generator.Overworld.scale1 = Vector(0.2,0.2,0.2)
 
 --- Generates an overworld area.
 -- @param self Overworld generator.
 -- @param pos Offset of the generated area.
 -- @param size Size of the generated area.
 Generator.Overworld.generate = function(self, pos, size)
+	-- Generate random rocks.
+	local m1 = Material:find{name = "granite1"}
+	local p1 = Vector()
+	local p2 = Vector()
+	local miny = pos.y * Voxel.tile_size
+	local maxy = (pos.y + size.y) * Voxel.tile_size
+	for x = 0,size.x-1 do
+		p1.x = pos.x + x
+		p2.x = (p1.x + 0.5) * Voxel.tile_size
+		for z = 0,size.z-1 do
+			p1.z = pos.z + z
+			p2.z = (p1.z + 0.5) * Voxel.tile_size
+			local height = Map.heightmap:get_height(p2, false)
+			if height and height >= miny and height < maxy then
+				p1.y = math.ceil(height * Voxel.tile_scale) - 1
+				local n1 = Noise:perlin_noise(p1, self.scale1, 1, 3, 0.5, Generator.inst.seed1)
+				if n1 > 0.8 then
+					Voxel:fill_region{point = p1, size = Vector(1, 1+(n1-0.8)/0.3, 1), tile = m1.id}
+				end
+			end
+		end
+	end
 	-- Generate objects.
 	local c = Vector()
 	for i=1,25 do
