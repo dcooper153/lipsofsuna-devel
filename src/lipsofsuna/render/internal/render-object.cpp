dcooper153/lipsofsuna-devel

commit 7e78907e70a2760f84eebae5f8609bb9d0d853bb

Made the view distance of static objects configurable.

src/lipsofsuna/render/internal/render-object.cpp
@@ -55,6 +55,7 @@ LIRenObject* liren_object_new (
 	self->render = render;
 	self->visible = 0;
 	self->shadow_casting = 0;
+	self->render_distance = -1.0f;
 
 	/* Choose a unique ID. */
 	while (!id)
@@ -307,6 +308,21 @@ void liren_object_update (
 		self->entity_build = NULL;
 	}
 
+	/* Hide objects too far away. */
+	/* Frustum culling cannot necessarily eliminate all desirable objects if the
+	   view distance is very low. Because of that, we allow the user to specify
+	   the render distance for the object. */
+	if (self->render_distance > 0)
+	{
+		float dist2 = self->node->getSquaredViewDepth (self->render->data->camera);
+		if (dist2 > self->render_distance * self->render_distance)
+			self->node->setVisible (false);
+		else
+			self->node->setVisible (true);
+	}
+	else
+		self->node->setVisible (true);
+
 	/* Send the pose to the entity. */
 	/* To reduce load when there are lots of animated objects, entities only
 	   update their skeletons when they are rendered. The pose tranformation is
@@ -476,6 +492,22 @@ int liren_object_set_realized (
 	self->visible = value;
 	self->node->setVisible (value);
 	return 1;
+}
+
+/**
+ * \brief Sets the render distance of the object.
+ * \param self Object.
+ * \param value Render distance.
+ */
+void liren_object_set_render_distance (
+	LIRenObject* self,
+	float        value)
+{
+	self->render_distance = value;
+	if (self->entity != NULL)
+		self->entity->setCastShadows (self->shadow_casting);
+	if (self->entity_build != NULL)
+		self->entity_build->setCastShadows (self->shadow_casting);
 }
 
 /**

data/lipsofsuna/client/objects/actor.lua
@@ -4,8 +4,8 @@ Actor.new = function(clss, args)
 	local self = Object.new(clss, args)
 	self.stats = {}
 	self.inventory:subscribe(self, function(args) self:update_inventory(args) end)
-	self.shadow_casting = Options.inst.shadow_casting_actors
 	Object.dict_active[self] = 1.0
+	Client.options:apply_object(self)
 	return self
 end
 

data/lipsofsuna/client/objects/item.lua
@@ -2,9 +2,9 @@ Item = Class(Object)
 
 Item.new = function(clss, args)
 	local self = Object.new(clss, args)
-	self.shadow_casting = Options.inst.shadow_casting_items
 	if args and args.spec and args.spec.special_effects then
 		Object.dict_active[self] = 0.1
 	end
+	Client.options:apply_object(self)
 	return self
 end

data/lipsofsuna/client/objects/obstacle.lua
@@ -2,9 +2,9 @@ Obstacle = Class(Object)
 
 Obstacle.new = function(clss, args)
 	local self = Object.new(clss, args)
-	self.shadow_casting = Options.inst.shadow_casting_obstacles
 	if args and args.spec and args.spec.special_effects then
 		Object.dict_active[self] = 0.1
 	end
+	Client.options:apply_object(self)
 	return self
 end

data/lipsofsuna/client/objects/staticobject.lua
@@ -1,10 +1,11 @@
 Staticobject = Class(Object)
+Staticobject.class_name = "Staticobject"
 
 Staticobject.new = function(clss, args)
 	local self = Object.new(clss, {id = args.id})
 	self.static = true
 	self.disable_saving = true
-	self.shadow_casting = Options.inst.shadow_casting_obstacles
 	for k,v in pairs(args) do self[k] = v end
+	Client.options:apply_object(self)
 	return self
 end

data/lipsofsuna/core/client/options.lua
@@ -15,6 +15,7 @@ Options.config_keys = {
 	join_account = {"Account name", "string"},
 	join_address = {"Server address", "string"},
 	join_port = {"Server port", "int", 1, 65535},
+	landmark_view_distance = {"Landmark view distance", "int", 1, 10},
 	master_server = {"Master server", "string"},
 	model_quality = {"High quality models", "bool"},
 	mouse_sensitivity = {"Mouse sensitivity", "float", 0, 1},
@@ -53,6 +54,7 @@ Options.new = function(clss)
 	self.join_account = "guest"
 	self.join_address = "localhost"
 	self.join_port = 10101
+	self.landmark_view_distance = 5
 	self.master_server = "http://lipsofsuna.org"
 	self.mouse_sensitivity = 1
 	self.mouse_smoothing = true
@@ -118,7 +120,29 @@ Options.new = function(clss)
 end
 
 Options.apply = function(self)
+	-- Set the anisotropic filter.
 	Render.anisotrophy = self.anisotropic_filter
+	-- Update the render properties of objects.
+	for k,v in pairs(Object.objects) do
+		self:apply_object(v)
+	end
+end
+
+Options.apply_object = function(self, object)
+	if object.class == Staticobject then
+		if self.landmark_view_distance < 10 then
+			object.render_distance = self.landmark_view_distance * 50
+		else
+			object.render_distance = nil
+		end
+		object.shadow_casting = self.shadow_casting_obstacles
+	elseif object.class == Obstacle then
+		object.shadow_casting = self.shadow_casting_obstacles
+	elseif object.class == Actor then
+		object.shadow_casting = self.shadow_casting_actors
+	else
+		object.shadow_casting = self.shadow_casting_items
+	end
 end
 
 Options.save = function(self)

data/lipsofsuna/ui/states/options.lua
@@ -27,25 +27,45 @@ Ui:add_widget{
 
 Ui:add_widget{
 	state = "options",
-	widget = function() return Widgets.Uiconfigoption("outlines_enabled",
-		function() Client:update_rendering_style() end) end}
+	widget = function()
+		return Widgets.Uiconfigoption("landmark_view_distance",
+			function() Client.options:apply() end)
+		end}
 
 Ui:add_widget{
 	state = "options",
-	widget = function() return Widgets.Uiconfigoption("shadow_casting_actors") end}
+	widget = function()
+		return Widgets.Uiconfigoption("outlines_enabled",
+			function() Client:update_rendering_style() end)
+		end}
 
 Ui:add_widget{
 	state = "options",
-	widget = function() return Widgets.Uiconfigoption("shadow_casting_items") end}
+	widget = function()
+		return Widgets.Uiconfigoption("shadow_casting_actors",
+			function() Client.options:apply() end)
+	end}
 
 Ui:add_widget{
 	state = "options",
-	widget = function() return Widgets.Uiconfigoption("shadow_casting_obstacles") end}
+	widget = function()
+		return Widgets.Uiconfigoption("shadow_casting_items",
+			function() Client.options:apply() end)
+	end}
 
 Ui:add_widget{
 	state = "options",
-	widget = function() return Widgets.Uiconfigoption("bloom_enabled",
-		function() Client:update_rendering_style() end) end}
+	widget = function()
+		return Widgets.Uiconfigoption("shadow_casting_obstacles",
+			function() Client.options:apply() end)
+	end}
+
+Ui:add_widget{
+	state = "options",
+	widget = function()
+		return Widgets.Uiconfigoption("bloom_enabled",
+			function() Client:update_rendering_style() end)
+	end}
 
 Ui:add_widget{
 	state = "options",

data/system/object-render.lua
@@ -97,37 +97,52 @@ end
 -- @name Object.particle_emitting
 -- @class table
 
+--- Specifies the maxium render distance of the object.
+-- @name Object.render_distance
+-- @class table
+
+--- Evaluates to true if the render data of the object has been loaded.
+-- @name Object.render_loaded
+-- @class table
+
 --- Toggles shadow casting for the object.
 -- @name Object.shadow_casting
 -- @class table
 
 Object:add_getters{
-	particle = function(s)
-		return rawget(s, "__particle")
+	particle = function(self)
+		return rawget(self, "__particle")
 	end,
-	particle_emitting = function(s)
-		return rawget(s, "__particle_emitting")
+	particle_emitting = function(self)
+		return rawget(self, "__particle_emitting")
+	end,
+	render_distance = function(self)
+		return rawget(self, "__render_distance")
 	end,
 	render_loaded = function(self)
 		return Los.object_get_render_loaded(self.handle)
 	end,
-	shadow_casting = function(s)
-		local v = rawget(s, "__shadow_casting")
+	shadow_casting = function(self)
+		local v = rawget(self, "__shadow_casting")
 		return (v ~= nil) and v or false
 	end}
 
 Object:add_setters{
-	particle = function(s, v)
-		rawset(s, "__particle", v)
-		rawset(s, "__particle_emitting", true)
-		rawset(s, "__model", nil)
-		Los.object_set_particle(s.handle, v)
+	particle = function(self, v)
+		rawset(self, "__particle", v)
+		rawset(self, "__particle_emitting", true)
+		rawset(self, "__model", nil)
+		Los.object_set_particle(self.handle, v)
 	end,
-	particle_emitting = function(s, v)
-		rawset(s, "__particle_emitting", v)
-		Los.object_set_particle_emitting(s.handle, v)
+	particle_emitting = function(self, v)
+		rawset(self, "__particle_emitting", v)
+		Los.object_set_particle_emitting(self.handle, v)
 	end,
-	shadow_casting = function(s, v)
-		rawset(s, "__shadow_casting", v)
-		Los.object_set_shadow_casting(s.handle, v)
+	render_distance = function(self, v)
+		rawset(self, "__render_distance", v)
+		Los.object_set_render_distance(self.handle, v)
+	end,
+	shadow_casting = function(self, v)
+		rawset(self, "__shadow_casting", v)
+		Los.object_set_shadow_casting(self.handle, v)
 	end}

commit bfc05a5125c94d54ccae5fe320bae1b46d0ffcc4

Fixed visibility management regression caused by commit 7e78907e.

src/lipsofsuna/render/internal/render-object.cpp
@@ -318,10 +318,10 @@ void liren_object_update (
 		if (dist2 > self->render_distance * self->render_distance)
 			self->node->setVisible (false);
 		else
-			self->node->setVisible (true);
+			self->node->setVisible (self->visible);
 	}
 	else
-		self->node->setVisible (true);
+		self->node->setVisible (self->visible);
 
 	/* Send the pose to the entity. */
 	/* To reduce load when there are lots of animated objects, entities only