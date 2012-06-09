3a19bac67690032e32b2438bb21a58778844eaa8
Ari Mustonen <amuzen@users.sourceforge.net>
2012-06-09 16:17:13 +0300

    Tuned lighting parameters a bit.

--- data/lipsofsuna/client/lighting.lua
+++ data/lipsofsuna/client/lighting.lua
@@ -4,8 +4,8 @@ Lighting.class_name = "Lighting"
 Lighting.init = function(self)
 	self.light_global = Light{diffuse={1,1,1,1}, directional = true, equation={1.9,0,0},
 	 shadow_casting = true}
-	--, spot_cutoff = 1.5, spot_exponent = 127}
 	self.light_spell = Light{diffuse={1,1,1,1}, equation={1.5,0,0.05}}
+	self.light_spell_timer = 0
 	Render.scene_ambient = {0.5,0.5,0.5}
 	-- Set the skybox.
 	Render.skybox = "skybox1"
@@ -13,14 +13,21 @@ end
 
 Lighting.update = function(self, secs)
 	-- Update the global light.
-	self.light_global.rotation = Quaternion(-0.16,-0.84,-0.32,0.42)
+	if self.dungeon_mode then
+		self.light_global.rotation = Quaternion(-0.68,0,0,0.82)
+	else
+		self.light_global.rotation = Quaternion(-0.16,-0.84,-0.32,0.42)
+	end
 	self.light_global.enabled = true
-	-- Update the light spell.
+	-- Update the light spell position.
 	if Client.player_object and self.light_spell.enabled then
 		local p = Client.player_object.position
+		self.light_spell_timer = self.light_spell_timer + secs
+		p.x = p.x + math.sin(self.light_spell_timer)
+		p.y = p.y + math.cos(0.9*self.light_spell_timer)
+		p.z = p.z + math.cos(-0.8*self.light_spell_timer)
 		local r = Client.player_object.rotation
-		self.light_spell.position = p + r * Vector(0,2,-1.5)
-		--self.light_spell.rotation = Quaternion{dir = p - self.light_spell.position, up = Vector(0, 1)}
+		self.light_spell.position = p + r * Vector(0,1.7,-1.5)
 	end
 end
 
@@ -32,8 +39,13 @@ Lighting.set_dungeon_mode = function(self, value)
 	if self.dungeon_mode == value then return end
 	self.dungeon_mode = value
 	-- Reconfigure the brightness of the global light.
-	self.light_global.equation = value and {2.1,0,0} or {1.9,0,0}
-	Render.scene_ambient = value and {0.2,0.2,0.2} or {0.5,0.5,0.5}
+	if value then
+		self.light_global.equation = {6,0,0}
+		Render.scene_ambient = {0.3,0.3,0.3}
+	else
+		self.light_global.equation = {1.9,0,0}
+		Render.scene_ambient = {0.5,0.5,0.5}
+	end
 	-- Reconfigure the visibility of the skybox.
 	Render.skybox = value and "" or "skybox1"
 end
