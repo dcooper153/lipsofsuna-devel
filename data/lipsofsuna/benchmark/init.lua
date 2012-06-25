dcooper153/lipsofsuna-devel

commit 558998b7f1adc032191f27e8e3ca2480009af8c2

Cleaned up movement prediction code.

data/lipsofsuna/client/movement-prediction.lua
@@ -1,0 +1,120 @@
+MovementPrediction = Class()
+MovementPrediction.class_name = "MovementPrediction"
+
+--- Creates a new movement predictor.
+-- @param clss Movement prediction class.
+-- @return Movement predictor.
+MovementPrediction.new = function(clss)
+	local self = Class.new(clss)
+	self.timer = 0
+	self.tick = 1/60
+	self.pred_position = Vector()
+	self.pred_rotation = Quaternion()
+	self.pred_tilt = 0
+	self.target_position = Vector()
+	self.target_rotation = Quaternion()
+	self.target_tilt = 0
+	self.target_velocity = Vector()
+	self.ipol_velocity = Vector()
+	self.position_max_error = 20
+	self.position_corr_rate = 0.2
+	self.rotation_corr_rate = 0.2
+	self.prediction_velocity_decay = 0.99
+	return self
+end
+
+--- Gets the current predicted position.
+-- @param self Movement prediction.
+-- @return Vector.
+MovementPrediction.get_predicted_position = function(self)
+	return self.pred_position
+end
+
+--- Gets the current predicted rotation.
+-- @param self Movement prediction.
+-- @return Quaternion.
+MovementPrediction.get_predicted_rotation = function(self)
+	return self.pred_rotation
+end
+
+--- Gets the current predicted tilt.
+-- @param self Movement prediction.
+-- @return Number.
+MovementPrediction.get_predicted_tilt = function(self)
+	return self.pred_tilt
+end
+
+--- Sets the target rotation.
+-- @param self Movement prediction.
+-- @param rot Rotation quaternion.
+-- @param tilt Tilt angle.
+MovementPrediction.set_target_rotation = function(self, rot, tilt)
+	self.target_rotation = rot
+	self.target_tilt = tilt or 0
+end
+
+--- Sets the target velocity.
+-- @param self Movement prediction.
+-- @param vel Velocity vector.
+MovementPrediction.set_target_velocity = function(self, vel)
+	self.target_velocity = vel or Vector()
+	self.ipol_timer = 0
+	self.ipol_velocity = self.target_velocity:copy()
+end
+
+--- Sets the target movement state.
+-- @param self Movement prediction.
+-- @param pos Position vector.
+-- @param rot Rotation quaternion.
+-- @param tilt Tilt angle.
+-- @param vel Velocity vector.
+MovementPrediction.set_target_state = function(self, pos, rot, tilt, vel)
+	-- Store the target state.
+	self.target_position = pos
+	self.target_rotation = rot
+	self.target_tilt = tilt or 0
+	self.target_velocity = vel or Vector()
+	-- Initialize the interpolation cycle.
+	self.ipol_timer = 0
+	self.ipol_velocity = self.target_velocity:copy()
+end
+
+--- Updates the movement prediction.
+-- @param self Movement prediction.
+-- @param secs Seconds since the last update.
+MovementPrediction.update = function(self, secs)
+	-- Update the timer.
+	if not self.ipol_timer then return self:warp() end
+	self.timer = self.timer + secs
+	-- Handle big warpings in time.
+	if self.timer > 1 then
+		return self:warp()
+	end
+	-- Handle big warpings in position.
+	if (self.target_position - self.pred_position).length > self.position_max_error then
+		return self:warp()
+	end
+	-- Update periodically to ensure frame rate independence.
+	while self.timer > self.tick do
+		self.timer = self.timer - self.tick
+		-- Damp velocity to reduce overshoots.
+		self.ipol_timer = self.ipol_timer + self.tick
+		self.ipol_velocity:multiply(self.prediction_velocity_decay)
+		-- Apply rotation smoothing over time.
+		self.pred_rotation = self.pred_rotation:nlerp(self.target_rotation, self.rotation_corr_rate)
+		self.pred_tilt = self.pred_tilt * (1 - self.rotation_corr_rate) + self.target_tilt * self.rotation_corr_rate
+		-- Apply position change predicted by the velocity.
+		self.pred_position:add(self.ipol_velocity * self.tick)
+		-- Correct position prediction errors over time.
+		self.pred_position:lerp(self.target_position, self.position_corr_rate)
+	end
+end
+
+--- Warps the predictor to the last known position.
+-- @param self Movement prediction.
+MovementPrediction.warp = function(self)
+	self.timer = 0
+	self.ipol_timer = 1
+	self.pred_position = self.target_position
+	self.pred_rotation = self.target_rotation
+end

data/lipsofsuna/client/network/object.lua
@@ -407,7 +407,8 @@ Protocol:add_handler{type = "OBJECT_SHOWN", func = function(event)
 	debug("  OK")
 	o.flags = Bitwise:band(flags, 0xFF)
 	o:set_model()
-	o:update_rotation(o.rotation, o.tilt)
+	o.prediction:set_target_state(o.position, o.rotation, o.tilt, Vector())
+	o.prediction:warp()
 	o.realized = true
 	-- Initialize speed lines.
 	if Bitwise:band(flags, Protocol.object_flags.SPEEDLINE) ~= 0 then

commit 960f29c99a2076a8d7e5abaae16c2160164afe18

Cleaned up object classes and fixed some regressions caused by 558998b7.

data/lipsofsuna/benchmark/init.lua
@@ -10,9 +10,9 @@ Benchmark.new = function(clss)
 	for i = 1,20 do
 		local o = Simulation:create_object_by_spec(spec)
 		local a = i/20*2*math.pi
-		o.position = Vector(500,500,500) + Vector(math.cos(a), 0, math.sin(a)):multiply(i/4)
+		o:set_motion_state(Vector(500,500,500) + Vector(math.cos(a), 0, math.sin(a)):multiply(i/4))
 		o:set_model()
-		o:animate_spec(anims[i % 3 + 1])
+		o:add_animation(anims[i % 3 + 1])
 		o.realized = true
 		self.objects[o.id] = o
 		self.object = o
@@ -55,7 +55,7 @@ Benchmark.update = function(self, secs)
 	Lighting:update(secs)
 	-- Modify objects.
 	for k,v in pairs(self.objects) do
-		Object.dict_active[v] = 5
+		v:activate(5)
 		if not v.model_rebuild_timer then
 			v.model_rebuild_timer = 2
 			v.body_style = {}

data/lipsofsuna/client/movement-prediction.lua
@@ -6,6 +6,7 @@ MovementPrediction.class_name = "MovementPrediction"
 -- @return Movement predictor.
 MovementPrediction.new = function(clss)
 	local self = Class.new(clss)
+	self.enabled = true
 	self.timer = 0
 	self.tick = 1/60
 	self.pred_position = Vector()
@@ -21,6 +22,14 @@ MovementPrediction.new = function(clss)
 	self.rotation_corr_rate = 0.2
 	self.prediction_velocity_decay = 0.99
 	return self
+end
+
+--- Enables or disables movement prediction.
+-- @param self Movement prediction.
+-- @param value True to enable.
+MovementPrediction.set_enabled = function(self, value)
+	self.enabled = value
+	if not value then self:warp() end
 end
 
 --- Gets the current predicted position.
@@ -51,6 +60,7 @@ end
 MovementPrediction.set_target_rotation = function(self, rot, tilt)
 	self.target_rotation = rot
 	self.target_tilt = tilt or 0
+	if not self.enabled then self:warp() end
 end
 
 --- Sets the target velocity.
@@ -77,12 +87,17 @@ MovementPrediction.set_target_state = function(self, pos, rot, tilt, vel)
 	-- Initialize the interpolation cycle.
 	self.ipol_timer = 0
 	self.ipol_velocity = self.target_velocity:copy()
+	if not self.enabled then self:warp() end
 end
 
 --- Updates the movement prediction.
 -- @param self Movement prediction.
 -- @param secs Seconds since the last update.
 MovementPrediction.update = function(self, secs)
+	-- Check if enabled.
+	if not self.enabled then
+		return self:warp()
+	end
 	-- Update the timer.
 	if not self.ipol_timer then return self:warp() end
 	self.timer = self.timer + secs

data/lipsofsuna/client/network/object.lua
@@ -3,7 +3,7 @@ Protocol:add_handler{type = "OBJECT_ANIMATED", func = function(event)
 	if not ok then return end
 	local o = Object:find{id = id}
 	if not o then return end
-	o:set_anim(anim, time)
+	o:add_animation(anim, time)
 end}
 
 Protocol:add_handler{type = "OBJECT_BEHEADED", func = function(event)
@@ -30,9 +30,7 @@ Protocol:add_handler{type = "OBJECT_DEAD", func = function(args)
 	-- Update death status.
 	if obj.dead == b then return end
 	obj.dead = b
-	if obj.rotation_real then
-		obj:update_rotation(obj.rotation_real)
-	end
+	obj:set_local_rotation()
 	if obj == Client.player_object then
 		Client:set_player_dead(b)
 	end
@@ -123,7 +121,7 @@ Protocol:add_handler{type = "OBJECT_FEAT", func = function(event)
 		animation = map[move] or animation
 	end
 	-- Play the animation.
-	obj:animate_spec(animation)
+	obj:add_animation(animation)
 end}
 
 Protocol:add_handler{type = "OBJECT_EFFECT", func = function(event)
@@ -281,7 +279,7 @@ Protocol:add_handler{type = "OBJECT_SHOWN", func = function(event)
 			local ok,anim,time = event.packet:resume("string", "float")
 			if not ok then return end
 			debug("    %s %f", anim, time)
-			o:set_anim(anim, time)
+			o:add_animation(anim, time)
 		end
 	end
 	-- Equipment.

data/lipsofsuna/client/objects/actor.lua
@@ -1,10 +1,12 @@
-Actor = Class(Object)
+require "client/objects/remote"
+
+Actor = Class(RemoteObject)
 
 Actor.new = function(clss, args)
-	local self = Object.new(clss, args)
+	local self = RemoteObject.new(clss, args)
 	self.stats = {}
 	self.inventory:subscribe(self, function(args) self:update_inventory(args) end)
-	Object.dict_active[self] = 1.0
+	self:activate(1)
 	Client.options:apply_object(self)
 	return self
 end
@@ -13,7 +15,7 @@ Actor.detach = function(self)
 	-- Hide equipment.
 	self.inventory:clear()
 	-- Call base.
-	Object.detach(self)
+	RemoteObject.detach(self)
 end
 
 --- Disables the equipment holding animation for the given slot.
@@ -32,7 +34,7 @@ end
 -- @param item Item added to the slot.
 Actor.enable_equipment_holding_animation = function(self, slot, item)
 	-- Enable the animation if supported by the actor.
-	local anim = self:animate_spec(item.spec.animation_hold)
+	local anim = self:add_animation(item.spec.animation_hold)
 	if anim then
 		if self.equipment_animations then
 			self.equipment_animations[slot] = anim
@@ -60,7 +62,7 @@ end
 Actor.set_model = function(self)
 	if self.spec.models then
 		-- Create a custom model.
-		Object.set_model(self, self.model)
+		RemoteObject.set_model(self, self.model)
 		self:request_model_rebuild()
 	else
 		-- Use a static model.
@@ -71,7 +73,7 @@ Actor.set_model = function(self)
 	-- Update body scale.
 	self:update_body_scale()
 	-- Update special effects
-	Object.set_model(self, true)
+	RemoteObject.set_model(self, true)
 	self.animated = true
 end
 
@@ -119,7 +121,7 @@ end
 -- @param secs Seconds since the last update.
 Actor.update = function(self, secs)
 	-- Call the base class.
-	Object.update(self, secs)
+	RemoteObject.update(self, secs)
 	-- Handle model rebuilding.
 	--
 	-- Equipment changes can occur frequently when newly appearing actors are
@@ -251,12 +253,12 @@ Actor.update_model = function(self)
 		spec = self.spec})
 end
 
---- Updates the rotation and tilt of the object.
+--- Sets the client overridden rotation and tilt of the object.
 -- @param self Object.
 -- @param quat Rotation quaternion.
 -- @param tilt Tilt angle in radians.
-Actor.update_rotation = function(self, quat, tilt)
-	Object.update_rotation(self, quat, tilt)
+Actor.set_local_rotation = function(self, quat, tilt)
+	RemoteObject.set_local_rotation(self, quat, tilt)
 	local spec = self.spec
 	if spec and spec.tilt_bone then
 		-- Calculate the tilting rotation.

data/lipsofsuna/client/objects/base.lua
@@ -1,0 +1,69 @@
+BaseObject = Class(Object)
+BaseObject.class_name = "BaseObject"
+
+--- Creates a new object.
+-- @param clss Base object class.
+-- @param args Arguments.
+-- @returns New object.
+BaseObject.new = function(clss, args)
+	local self = Object.new(clss, args)
+	return self
+end
+
+--- Detaches the object and its anchors from the scene.
+-- @param self Object.
+BaseObject.detach = function(self)
+	-- Hide special effects.
+	if self.special_effects then
+		for k,v in pairs(self.special_effects) do
+			if v.enabled then
+				v.enabled = false
+			else
+				v.realized = false
+			end
+		end
+		self.special_effects = nil
+	end
+	-- Stop the speed line.
+	if self.speedline then
+		self.speedline.object = nil
+	end
+	-- Hide self.
+	self.realized = false
+	Object.dict_active[self] = nil
+end
+
+--- Updates the object.
+-- @param self Object.
+-- @param secs Seconds since the last update.
+BaseObject.update = function(self, secs)
+	if not self.realized then return end
+	if self.special_effects then
+		for k,v in pairs(self.special_effects) do
+			local p = self.position
+			local r = self.rotation
+			if v.node then
+				local np,nr = self:find_node{name = v.node}
+				if np then
+					p = p + r * np
+					r = r * nr
+				end
+			end
+			if v.offset then p = p + v.offset end
+			if v.rotate then v.rotation = r end
+			v.position = p
+		end
+	end
+end
+
+-- FIXME: Shouldn't be here.
+Object.physics_position_correction = Vector(0, 0, 0)
+if not Object.animate then
+	Object.animate = function() end
+end
+if not Object.find_node then
+	Object.find_node = function() end
+end
+if not Object.get_animation then
+	Object.get_animation = function() end
+end

data/lipsofsuna/client/objects/item.lua
@@ -1,9 +1,11 @@
-Item = Class(Object)
+require "client/objects/remote"
+
+Item = Class(RemoteObject)
 
 Item.new = function(clss, args)
-	local self = Object.new(clss, args)
+	local self = RemoteObject.new(clss, args)
 	if args and args.spec and args.spec.special_effects then
-		Object.dict_active[self] = 0.1
+		self:activate(0.1)
 	end
 	Client.options:apply_object(self)
 	return self

data/lipsofsuna/client/objects/object.lua
@@ -1,186 +1,0 @@
-require "client/movement-prediction"
-require "client/objects/speedline"
-
-Object.dict_active = setmetatable({}, {__mode = "k"})
-Object.physics_position_correction = Vector(0, 0, 0)
-
-local oldinit = Object.new
-Object.new = function(clss, args)
-	local self = oldinit(clss, args)
-	self.inventory = Inventory{id = self.id}
-	self.prediction = MovementPrediction()
-	return self
-end
-
-Object.set_active_time = function(self, duration)
-	Object.dict_active[self] = duration
-end
-
-Object.add_speedline = function(self, args)
-	-- Stop the old speedline
-	if self.speedline then
-		self.speedline.object = nil
-		self.speedline = nil
-	end
-	-- Create the new speedline
-	if not args then args = {} end
-	args.object = self
-	self.speedline = Speedline(args)
-end
-
-Object.animate_spec = function(self, name)
-	return self:set_anim(name)
-end
-
-Object.detach = function(self)
-	-- Hide special effects.
-	if self.special_effects then
-		for k,v in pairs(self.special_effects) do
-			if v.enabled then
-				v.enabled = false
-			else
-				v.realized = false
-			end
-		end
-		self.special_effects = nil
-	end
-	-- Stop the speed line.
-	if self.speedline then
-		self.speedline.object = nil
-	end
-	-- Hide self.
-	self.realized = false
-	Object.dict_active[self] = nil
-end
-
-Object.set_anim = function(self, name, time)
-	-- Play the animation.
-	local args = {animation = name, fade_in = 0.3, fade_out = 0.3, time = time}
-	local anim = self.spec and self.spec.get_animation and self.spec:get_animation(name)
-	if anim then
-		for k,v in pairs(anim:get_arguments()) do args[k] = v end
-	end
-	self:animate(args)
-	-- Mark as active.
-	self.animated = true
-	Object.dict_active[self] = 5.0
-	return args
-end
-
-Object.set_dialog = function(self, type, args)
-	-- Update the dialog state.
-	if type == "choice" then
-		self.dialog = {type = type, choices = args}
-	elseif type == "message" then
-		self.dialog = {type = type, character = args.character, message = args.message}
-	else
-		self.dialog = nil
-	end
-	-- Update the dialog UI.
-	if Client.active_dialog_object == self then
-		Ui:restart_state()
-	end
-end
-
-Object.update = function(self, secs)
-	-- Update special effects.
-	if self.realized then
-		if self.special_effects then
-			for k,v in pairs(self.special_effects) do
-				local p = self.position
-				local r = self.rotation
-				if v.node then
-					local np,nr = self:find_node{name = v.node}
-					if np then
-						p = p + r * np
-						r = r * nr
-					end
-				end
-				if v.offset then p = p + v.offset end
-				if v.rotate then v.rotation = r end
-				v.position = p
-			end
-		end
-	end
-end
-
---- Sets the model and special effects of the object from its spec.
--- @param self Object.
--- @param skip True to only set the special effects.
-Object.set_model = function(self, skip)
-	if not self.spec then return end
-	-- Set the new model.
-	if not skip then
-		local shape = self.shape
-		self.model = Model:find_or_load{file = self.spec.model}
-		self.shape = shape
-	end
-	-- Detach old special effects.
-	if self.special_effects then
-		for k,v in pairs(self.special_effects) do v:detach() end
-		self.special_effects = nil
-	end
-	self.special_effects = nil
-	-- Create new special effects.
-	local effects = self.spec.get_special_effects and self.spec:get_special_effects()
-	if effects then
-		self.special_effects = {}
-		for k,v in pairs(effects) do
-			if v.light then
-				local fx = Light{ambient = v.light_ambient, diffuse = v.light_diffuse, node = v.node,
-					equation = v.light_equation, position = self.position, enabled = true}
-				table.insert(self.special_effects, fx)
-			end
-			if v.particle then
-				local fx = Object{particle = v.particle, node = v.node, position = self.position, realized = true}
-				table.insert(self.special_effects, fx)
-			end
-		end
-	end
-end
-
---- Rebuilds the character model of the object.
--- @param self Object.
-Object.update_model = function(self)
-end
-
---- Plays footstep sounds for actors.
--- @param self Object.
--- @param secs Seconds since the last update.
-Object.update_sound = function(self, secs)
-end
-
---- Updates the rotation and tilt of the object.
--- @param self Object.
--- @param quat Rotation quaternion.
--- @param tilt Tilt angle in radians.
-Object.update_rotation = function(self, quat, tilt)
-	self.rotation = quat
-	self.tilt = tilt
-	self.prediction:set_target_rotation(quat, tilt)
-end
-
-Object.update_motion_state = function(self, secs)
-	self.prediction:update(secs)
-	self.position = self.prediction:get_predicted_position()
-	if self.dead or self ~= Client.player_object then
-		self.rotation = self.prediction:get_predicted_rotation()
-		self.tilt = self.prediction:get_predicted_tilt()
-	end
-end
-
-Object.set_motion_state = function(self, pos, rot, vel, tilt)
-	self.prediction:set_target_state(pos, rot, tilt, vel)
-	Object.dict_active[self] = 5.0
-end
-
--- Dummy functions for testing without object rendering.
-if not Object.animate then
-	Object.animate = function() end
-end
-if not Object.find_node then
-	Object.find_node = function() end
-end
-if not Object.get_animation then
-	Object.get_animation = function() end
-end

data/lipsofsuna/client/objects/obstacle.lua
@@ -1,9 +1,11 @@
-Obstacle = Class(Object)
+require "client/objects/remote"
+
+Obstacle = Class(RemoteObject)
 
 Obstacle.new = function(clss, args)
-	local self = Object.new(clss, args)
+	local self = RemoteObject.new(clss, args)
 	if args and args.spec and args.spec.special_effects then
-		Object.dict_active[self] = 0.1
+		self:activate(0.1)
 	end
 	Client.options:apply_object(self)
 	return self

data/lipsofsuna/client/objects/remote.lua
@@ -1,0 +1,137 @@
+require "client/movement-prediction"
+require "client/objects/base"
+require "client/objects/speedline"
+
+RemoteObject = Class(BaseObject)
+RemoteObject.class_name = "RemoteObject"
+
+--- Creates a new object that can be synchronized with a server side object.
+-- @param clss Remote object class.
+-- @param args Arguments.
+-- @returns New object.
+RemoteObject.new = function(clss, args)
+	local self = BaseObject.new(clss, args)
+	self.inventory = Inventory{id = self.id}
+	self.prediction = MovementPrediction()
+	return self
+end
+
+--- Adds an animation from the spec of the object by name.
+-- @param self Object.
+-- @param name Animation name.
+-- @param time Optional start time, or nil.
+-- @return Animation arguments.
+RemoteObject.add_animation = function(self, name, time)
+	-- Play the animation.
+	local args = {animation = name, fade_in = 0.3, fade_out = 0.3, time = time}
+	local anim = self.spec and self.spec.get_animation and self.spec:get_animation(name)
+	if anim then
+		for k,v in pairs(anim:get_arguments()) do args[k] = v end
+	end
+	self:animate(args)
+	-- Mark as active.
+	self.animated = true
+	self:activate(5)
+	return args
+end
+
+--- Adds a speed line effect for the object.
+-- @param self Object.
+-- @param args Speed line construction arguments.
+RemoteObject.add_speedline = function(self, args)
+	-- Stop the old speedline
+	if self.speedline then
+		self.speedline.object = nil
+		self.speedline = nil
+	end
+	-- Create the new speedline
+	if not args then args = {} end
+	args.object = self
+	self.speedline = Speedline(args)
+end
+
+--- Sets the dialog state of the object.
+-- @param self Object.
+-- @param type Dialog type. ("choice"/"message"/nil)
+-- @param args List of choices, the message arguments, or nil.
+RemoteObject.set_dialog = function(self, type, args)
+	-- Update the dialog state.
+	if type == "choice" then
+		self.dialog = {type = type, choices = args}
+	elseif type == "message" then
+		self.dialog = {type = type, character = args.character, message = args.message}
+	else
+		self.dialog = nil
+	end
+	-- Update the dialog UI.
+	if Client.active_dialog_object == self then
+		Ui:restart_state()
+	end
+end
+
+--- Sets the model and special effects of the object from its spec.
+-- @param self Object.
+-- @param skip True to only set the special effects.
+RemoteObject.set_model = function(self, skip)
+	if not self.spec then return end
+	-- Set the new model.
+	if not skip then
+		local shape = self.shape
+		self.model = Model:find_or_load{file = self.spec.model}
+		self.shape = shape
+	end
+	-- Detach old special effects.
+	if self.special_effects then
+		for k,v in pairs(self.special_effects) do v:detach() end
+		self.special_effects = nil
+	end
+	self.special_effects = nil
+	-- Create new special effects.
+	local effects = self.spec.get_special_effects and self.spec:get_special_effects()
+	if effects then
+		self.special_effects = {}
+		for k,v in pairs(effects) do
+			if v.light then
+				local fx = Light{ambient = v.light_ambient, diffuse = v.light_diffuse, node = v.node,
+					equation = v.light_equation, position = self.position, enabled = true}
+				table.insert(self.special_effects, fx)
+			end
+			if v.particle then
+				local fx = Object{particle = v.particle, node = v.node, position = self.position, realized = true}
+				table.insert(self.special_effects, fx)
+			end
+		end
+	end
+end
+
+--- Rebuilds the character model of the object.
+-- @param self Object.
+RemoteObject.update_model = function(self)
+end
+
+--- Plays footstep sounds for actors.
+-- @param self Object.
+-- @param secs Seconds since the last update.
+RemoteObject.update_sound = function(self, secs)
+end
+
+--- Sets the client overridden rotation and tilt of the object.
+-- @param self Object.
+-- @param quat Rotation quaternion.
+-- @param tilt Tilt angle in radians.
+RemoteObject.set_local_rotation = function(self, quat, tilt)
+	self.rotation = quat
+	self.tilt = tilt
+	self.prediction:set_target_rotation(quat, tilt)
+end
+
+--- Sets the target motion state for movement prediction.
+-- @param self Object.
+-- @param pos Position vector.
+-- @param rot Rotation quaternion.
+-- @param vel Velocity vector.
+-- @param tilt Tilt angle.
+RemoteObject.set_motion_state = function(self, pos, rot, vel, tilt)
+	self.prediction:set_target_state(pos, rot or Quaternion(), tilt, vel)
+	self:activate(5)
+end

data/lipsofsuna/client/objects/speedline.lua
@@ -19,7 +19,7 @@ Speedline.new = function(clss, args)
 	self.duration = args.duration or 10000
 	self.model = Model()
 	self.model:add_material{material = "speedline1"}
-	self:set_active_time(self.delay + self.duration + 3)
+	self:activate(self.delay + self.duration + 3)
 	return self
 end
 
@@ -61,8 +61,6 @@ Speedline.update = function(self, secs)
 		-- Realize the effect.
 		if not self.realized then
 			self.position = self.object.position
-			self.prediction:set_target_state(self.position, self.rotation, 0, Vector())
-			self.prediction:warp()
 			self.realized = true
 		end
 		-- Get the current line vertices.

data/lipsofsuna/client/objects/spell.lua
@@ -1,8 +1,10 @@
-Spell = Class(Object)
+require "client/objects/remote"
+
+Spell = Class(RemoteObject)
 Spell.class_name = "Spell"
 
 Spell.new = function(clss, args)
-	local self = Object.new(clss, args)
+	local self = RemoteObject.new(clss, args)
 	if args and args.spec then
 		if args.spec.particle then
 			self.particle = args.spec.particle
@@ -21,13 +23,13 @@ Spell.detach = function(self, soft)
 		self.particle_emitting = false
 	else
 		-- Forced instant removal.
-		Object.detach(self)
+		RemoteObject.detach(self)
 	end
 end
 
 Spell.update = function(self, secs)
 	-- Call base update.
-	Object.update(self, secs)
+	RemoteObject.update(self, secs)
 	-- Handle delayed destruction.
 	if self.destroy_timer then
 		self.destroy_timer = self.destroy_timer - secs

data/lipsofsuna/client/objects/staticobject.lua
@@ -1,8 +1,10 @@
-Staticobject = Class(Object)
+require "client/objects/remote"
+
+Staticobject = Class(RemoteObject)
 Staticobject.class_name = "Staticobject"
 
 Staticobject.new = function(clss, args)
-	local self = Object.new(clss, {id = args.id})
+	local self = RemoteObject.new(clss, {id = args.id})
 	self.static = true
 	self.disable_saving = true
 	for k,v in pairs(args) do self[k] = v end

data/lipsofsuna/client/operators/chargen.lua
@@ -201,7 +201,7 @@ Operators.chargen.update = function(self, secs)
 		object.skin_color = Color:hsv_to_rgb(self.char.skin_color)
 		object.skin_style = self.char.skin_style
 		object:set_model()
-		object:animate_spec("idle")
+		object:add_animation("idle")
 		if self.dump_presets then
 			print(self.data.object:write_preset())
 		end

data/lipsofsuna/client/player.lua
@@ -84,7 +84,7 @@ Player.update_rotation = function(clss, secs)
 	clss.tilt = 0
 	-- Update rotation.
 	local rot = Quaternion{euler = {clss.turn_state, 0, 0}}
-	clss.object:update_rotation(rot, -clss.tilt_state)
+	clss.object:set_local_rotation(rot, -clss.tilt_state)
 	-- Sync rotation with the server.
 	-- Rotation takes at most 0.25 seconds to fully synchronize. Large changes
 	-- are sent immediately whereas smaller changes are grouped together to

data/lipsofsuna/client/simulation.lua
@@ -68,14 +68,21 @@ Simulation.update = function(self, secs)
 			k:update_sound(secs)
 		end
 		-- Interpolate positions.
-		k:update_motion_state(secs)
+		if k.prediction and k.prediction.enabled then
+			k.prediction:update(secs)
+			k.position = k.prediction:get_predicted_position()
+			if k.dead or k ~= Client.player_object then
+				k.rotation = k.prediction:get_predicted_rotation()
+				k.tilt = k.prediction:get_predicted_tilt()
+			end
+		end
 		-- Update slots and special effects.
 		k:update(secs)
 		-- Maintain activity.
 		if k.spec and k.spec.type ~= "actor" then
 			v = v - secs
 			if v <= 0 then v = nil end
-			Object.dict_active[k] = v
+			k:activate(v)
 		end
 	end
 end

data/lipsofsuna/editor/editorobject.lua
@@ -1,28 +1,7 @@
-EditorObject = Class(Object)
+require "client/objects/base"
+
+EditorObject = Class(BaseObject)
 EditorObject.class_name = "EditorObject"
-
-EditorObject:add_getters{
-	spec = function(s) return rawget(s, "__spec") end}
-
-EditorObject:add_setters{
-	spec = function(s, v)
-		rawset(s, "__spec", v)
-		s.model = v.model
-		if s.model and v.models then
-			local m = s.model:copy()
-			for k,v in pairs(v.models) do
-				if k ~= "skeleton" then
-					m:merge(Model:find_or_load{file = v})
-				end
-			end
-			m:calculate_bounds()
-			m:changed()
-			s.model = m
-		end
-		if v.type == "actor" then
-			s:animate{animation = "idle", channel = 1, permanent = true}
-		end
-	end}
 
 EditorObject.move = function(self, value, step)
 	self.position = self.position + value * step
@@ -50,3 +29,26 @@ EditorObject.rotate = function(self, value, steps)
 	e[1] = math.floor(e[1] / s + value + 0.5) * s
 	self.rotation = Quaternion{euler = e}
 end
+
+EditorObject:add_getters{
+	spec = function(s) return rawget(s, "__spec") end}
+
+EditorObject:add_setters{
+	spec = function(s, v)
+		rawset(s, "__spec", v)
+		s.model = v.model
+		if s.model and v.models then
+			local m = s.model:copy()
+			for k,v in pairs(v.models) do
+				if k ~= "skeleton" then
+					m:merge(Model:find_or_load{file = v})
+				end
+			end
+			m:calculate_bounds()
+			m:changed()
+			s.model = m
+		end
+		if v.type == "actor" then
+			s:animate{animation = "idle", channel = 1, permanent = true}
+		end
+	end}

data/system/object.lua
@@ -11,6 +11,7 @@ end
 Object = Class()
 Object.class_name = "Object"
 Object.objects = setmetatable({}, {__mode = "v"})
+Object.dict_active = setmetatable({}, {__mode = "k"})
 
 --- Creates a new object.
 -- @param clss Object class.
@@ -86,10 +87,24 @@ Object.get_free_id = function(clss)
 	end
 end
 
+--- Adds the object to the list of active objects.
+-- @param self Objects.
+-- @param secs Number of seconds to remain active.
+Object.activate = function(self, secs)
+	Object.dict_active[self] = secs
+end
+
 --- Recalculates the bounding box of the model of the object.
 -- @param self Object.
 Object.calculate_bounds = function(self)
 	Los.object_calculate_bounds(self.handle)
+end
+
+--- Detaches the object from the scene.
+-- @param self Object.
+Object.detach = function(self)
+	self.realized = false
+	Object.dict_active[self] = nil
 end
 
 --- Prevents map sectors around the object from being unloaded.