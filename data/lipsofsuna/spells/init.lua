b86e3a860952015e2ed73f3fa175487cd51c54bb
Ari Mustonen <amuzen@users.sourceforge.net>
2013-06-30 18:13:05 +0300

    Renamed spells/influences to spells/modifiers for consistency.

--- data/lipsofsuna/spells/init.lua
+++ data/lipsofsuna/spells/init.lua
@@ -2,7 +2,7 @@ local File = require("system/file")
 local Program = require("system/core")
 
 Program:add_path(Mod.path .. "effects")
-File:require_directory(Mod.path .. "influences")
+File:require_directory(Mod.path .. "modifiers")
 
 Spellspec{
 	name = "fireball1",
