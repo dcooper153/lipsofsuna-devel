ec720a3eed139d2908d130fa1342931bcf5a707f
Ari Mustonen <amuzen@users.sourceforge.net>
2013-07-15 21:35:55 +0300

    The terrain insertion action sort of works again.

--- data/lipsofsuna/core/actions/build.lua
+++ data/lipsofsuna/core/actions/build.lua
@@ -1,47 +1,49 @@
-local Coroutine = require("system/coroutine")
+local Combat = require("core/server/combat")
 local Physics = require("system/physics")
 
-local perform_attack = function(attacker)
-	do return end --FIXME
-	local weapon = attacker:get_weapon()
-	if not weapon then return end
-	Coroutine(function(t)
-		-- Play the attack effect.
-		Main.vision:object_effect(attacker, "swing1")
-		Main.vision:object_event(attacker, "object attack", {move = "stand", variant = math.random(0, 255)})
-		Coroutine:sleep(attacker.spec.timing_build * 0.02)
+Actionspec{
+	name = "build",
+	categories = { ["build"] = true },
+	start = function(action, move)
+		if action.object.cooldown then return end
+		-- Initialize timing.
+		action.time = 0
+		action.duration = action.object.spec.timing_build * 0.02
+		-- Play the start effect.
+		Main.vision:object_effect(action.object, "swing1")
+		Main.vision:object_event(action.object, "object attack", {move = "stand", variant = math.random(0, 255)})
+		-- Enable effect-over-time updates.
+		return true
+	end,
+	update = function(action, secs)
+		-- Wait for the delay to end.
+		action.object.cooldown = 0.1
+		action.time = action.time + secs
+		if action.time <= action.duration then return true end
 		-- Check for a correct weapon.
+		local weapon = action.object:get_weapon()
+		if not weapon then return end
 		if not weapon.spec.construct_tile then return end
 		local material = TerrainMaterialSpec:find_by_name(weapon.spec.construct_tile)
 		if not material then return end
 		-- Perform the ray cast.
-		local src,dst = attacker:get_attack_ray()
+		local src,dst = action.object:get_attack_ray()
 		local r = Physics:cast_ray(src, dst)
 		if not r or r.object then return end
 		-- Check for sufficient materials.
-		local radius = 1.2
-		local materials = Main.terrain.terrain:count_materials_in_sphere(r.point, radius)
+		local radius = 0.2
+		local terr = Main.terrain.terrain
+		local tile = Vector(r.point.x / terr.grid_size, r.point.y, r.point.z / terr.grid_size)
+		local materials = terr:count_column_materials(tile.x, tile.z, tile.y - radius, 2 * radius)
 		local need = materials[0]
 		if not need then return end
+		need = math.floor(10 * need) / 10
 		if need > weapon:get_count() then return end
 		-- Build the tile.
 		weapon:subtract(need)
-		local p
-		if r.normal.y > 0 then
-			p = Vector(0,-radius/4,0):add(r.point)
-		else
-			p = Vector(0,radius/4,0):add(r.point)
-		end
-		Main.terrain.terrain:add_sphere_filter_id(p, radius, material.id, 0)
+		terr:add_stick_filter_id(tile.x, tile.z, tile.y - radius, 2 * radius, material.id, 0)
 		Main.vision:world_effect(r.point, material.effect_build)
-	end)
-end
-
-Actionspec{
-	name = "build",
-	charge_start = function(user)
-		perform_attack(user)
 	end,
-	func = function(feat, info, args)
-		perform_attack(args.user)
+	get_score = function(action)
+		return 1
 	end}
