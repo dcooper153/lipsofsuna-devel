bc2de4944f36577543568ae47d5fef87788b8c18
Ari Mustonen <amuzen@users.sourceforge.net>
2013-07-27 23:06:26 +0300

    Fixed combat utils missing from the dedicated server.

--- data/lipsofsuna/arena/main.lua
+++ data/lipsofsuna/arena/main.lua
@@ -1,39 +1,5 @@
-local Arena = require("arena/arena")
-local ArenaCamera = require("arena/camera")
-local Client = require("core/client/client")
 local CombatUtils = require("arena/combat-utils")
-local Hooks = require("system/hooks")
-local Ui = require("ui/ui")
-
-Main.game_modes:register("Arena", function()
-	Main.arena = Arena()
-	Ui:set_state("arena")
-end)
-
-Main.game_end_hooks:register(0, function(secs)
-	if Main.arena then
-		Main.arena:close()
-		Main.arena = nil
-	end
-end)
 
 Main.main_start_hooks:register(0, function(secs)
 	Main.combat_utils = CombatUtils()
-	Main.objects.object_created_hooks:register(15, function(object)
-		--FIXME
-		if Main.arena then
-			object.has_server_data = function() return true end
-		end
-	end)
-end)
-
-Main.update_hooks:register(0, function(secs)
-	if Main.arena then
-		Main.timing:start_action("arena")
-		Main.arena:update(secs)
-	end
-end)
-
-Client:register_init_hook(26, function()
-	Client.camera_manager:register_camera("arena", ArenaCamera())
 end)
