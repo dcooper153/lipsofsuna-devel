2a8e792d75576a5270365e7d6818390c0d1f6917
Ari Mustonen <amuzen@users.sourceforge.net>
2013-07-28 13:19:15 +0300

    Fixed crafting not working in multiplayer.

---
+++ data/lipsofsuna/crafting/main-client.lua
@@ -0,0 +1,35 @@
+local Client = require("core/client/client")
+local Crafting = require("crafting/crafting")
+local CraftingCamera = require("crafting/camera")
+
+Main.game_modes:register("Crafting", function()
+	Main.crafting = Crafting()
+	Ui:set_state("crafting")
+end)
+
+Main.game_end_hooks:register(0, function(secs)
+	if Main.crafting then
+		Main.crafting:close()
+		Main.crafting = nil
+	end
+end)
+
+Main.main_start_hooks:register(0, function(secs)
+	Main.objects.object_created_hooks:register(15, function(object)
+		--FIXME
+		if Main.crafting then
+			object.has_server_data = function() return true end
+		end
+	end)
+end)
+
+Main.update_hooks:register(0, function(secs)
+	if Main.crafting then
+		Main.timing:start_action("crafting")
+		Main.crafting:update(secs)
+	end
+end)
+
+Client:register_init_hook(26, function()
+	Client.camera_manager:register_camera("crafting", CraftingCamera())
+end)
