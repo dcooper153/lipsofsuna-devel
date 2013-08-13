01a8d8cdbc57b03d0a3f3ecc76523d7fa892467b
Ari Mustonen <amuzen@users.sourceforge.net>
2013-08-13 21:03:11 +0300

    Fixed the tackle action used by slimes and imps.

--- data/lipsofsuna/core/actions/tackle.lua
+++ data/lipsofsuna/core/actions/tackle.lua
@@ -1,8 +1,10 @@
-local Coroutine = require("system/coroutine")
+local Damage = require("arena/damage")
 
 Actionspec{
 	name = "tackle",
-	func = function(feat, info, args)
+	categories = { ["melee"] = true },
+	start = function(action, move)
+		if action.object.cooldown then return end
 		-- Setup the collision callback.
 		local ground_hits = 0
 		local callback = function(self, result)
@@ -12,37 +14,59 @@ Actionspec{
 				if ground_hits < 30 then return end
 			end
 			-- Stop the tackle motion.
-			args.user.physics:set_contact_events(false)
+			action.object.physics:set_contact_events(false)
 			self.contact_cb = nil
-			-- Apply the feat effects.
-			local args = {
-				object = result.object,
-				owner = self,
-				point = result.point,
-				tile = result.tile}
-			feat:play_effects_impact(args)
-			feat:apply_touch(args)
+			-- Calculate the damage.
+			local defender = result.object
+			local damage = Damage()
+			damage:add_unarmed_modifiers(self.skills)
+			damage:apply_attacker_physical_modifiers(self)
+			damage:apply_defender_armor(defender)
+			damage:apply_defender_blocking(defender)
+			damage:apply_defender_vulnerabilities(defender)
+			-- Apply the damage.
+			Main.combat_utils:apply_damage(self, defender, result.tile, damage, result.point)
 			-- Bounce the target strongly.
-			if result.object then
-				result.object.physics:impulse(Vector(0,400,-400):transform(self:get_rotation()))
+			if defender then
+				defender.physics:impulse(Vector(0,400,-400):transform(self:get_rotation()))
 			end
 		end
-		args.user.contact_cb = callback
-		args.user.physics:set_contact_events(true)
+		action.object.contact_cb = callback
+		action.object.physics:set_contact_events(true)
 		-- Initialize the tackle motion
-		args.user:action("jump")
-		local vel = args.user:get_rotation() * Vector(0,0.25,-1) * 10
-		args.user:set_velocity(vel)
-		feat:play_effects(args)
-		-- Maintain the tackle motion.
-		Coroutine(function(t)
-			for i = 1,10 do
-				Coroutine:sleep(0.1)
-				if not args.user:get_visible() then break end
-				if args.user.contact_cb ~= callback then break end
-				args.user:set_velocity(vel)
-			end
-			args.user.physics:set_contact_events(false)
-			args.user.contact_cb = nil
-		end)
+		action.object:action("jump")
+		action.velocity = action.object:get_rotation() * Vector(0,0.25,-1) * 10
+		action.object:set_velocity(action.velocity)
+		--feat:play_effects(args)
+		-- Initialize timing.
+		action.timer = 0
+		action.updates = 0
+		-- Enable effect-over-time updates.
+		return true
+	end,
+	update = function(action, secs)
+		-- Check for cancellation.
+		action.object.cooldown = 1 + 3 * math.random()
+		if not action.object:get_visible() then return end
+		if action.object.contact_cb ~= callback then return end
+		-- Wait for the next tick.
+		action.timer = action.timer + secs
+		if action.timer < 0.1 then return true end
+		action.timer = action.timer - 0.1
+		-- Check if done.
+		action.updates = action.updates + 1
+		if action.updates == 10 then
+			action.object.physics:set_contact_events(false)
+			action.object.contact_cb = nil
+			return
+		end
+		-- Maintain the velocity.
+		action.object:set_velocity(action.velocity)
+		return true
+	end,
+	get_range = function(action)
+		return 10
+	end,
+	get_score = function(action)
+		return 1
 	end}
