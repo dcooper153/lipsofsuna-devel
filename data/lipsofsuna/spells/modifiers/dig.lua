50f8cb70e393bff15a46c2e40e15041c55fbb858
Ari Mustonen <amuzen@users.sourceforge.net>
2013-10-20 19:13:56 +0300

    Fixed the dig spell.

--- data/lipsofsuna/spells/modifiers/dig.lua
+++ data/lipsofsuna/spells/modifiers/dig.lua
@@ -1,3 +1,4 @@
+local BuildingUtils = require("building/building-utils")
 local ModifierSpec = require("core/specs/modifier")
 
 -- Dig terrain.
@@ -8,5 +9,12 @@ local DigModifier = ModifierSpec:find_by_name("dig")
 -- @param value Strength of the modifier.
 -- @return True to enable effect-over-time updates. False otherwise.
 DigModifier.start = function(modifier, value)
-	-- TODO
+	BuildingUtils:destroy_terrain_sphere(modifier.owner, modifier.point, 3)
+end
+
+--- Applies the modifier.
+-- @param modifier Modifier.
+-- @param value Strength of the modifier.
+DigModifier.start_terrain = function(modifier, value)
+	DigModifier.start(modifier, value)
 end
