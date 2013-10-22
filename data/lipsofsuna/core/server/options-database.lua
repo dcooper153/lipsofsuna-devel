0e3b6f4e23b2cba0003cd05a3aee17625a014a36
Ari Mustonen <amuzen@users.sourceforge.net>
2013-10-23 01:19:24 +0300

    Changed the save file version due to major map generator changes.

--- data/lipsofsuna/core/server/options-database.lua
+++ data/lipsofsuna/core/server/options-database.lua
@@ -21,7 +21,7 @@ local OptionsDatabase = Class("OptionsDatabase")
 OptionsDatabase.new = function(clss, db)
 	local self = Class.new(clss)
 	self.__db = db
-	self.__version = "8"
+	self.__version = "9"
 	return self
 end
 
