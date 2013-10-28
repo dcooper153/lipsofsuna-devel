76b11d203df3186150037ed75705687a3a342db0
Ari Mustonen <amuzen@users.sourceforge.net>
2013-10-29 01:03:03 +0200

    Added a new placeholder texture for the stone imp.

--- data/lipsofsuna/core/misc/models.lua
+++ data/lipsofsuna/core/misc/models.lua
@@ -3,7 +3,7 @@
 
 local ModelSpec = require("core/specs/model")
 
-for k,v in pairs({"aerarmplate1", "aerbra1", "aerbloomers1", "aerbloomerstop1", "aerchinadress1", "aerfedora1", "aerheadband1", "aerleatherchest1", "aerleatherskirt1", "aerlegarmor2", "aerleggings1", "aermale1arms1", "aermale1boots1", "aermale1chest1", "aermale1eyes1", "aermale1feet1", "aermale1hair1", "aermale1hands1", "aermale1head1", "aermale1leatherarms2", "aermale1leatherchest1", "aermale1leatherchest2", "aermale1leatherhead2", "aermale1leatherlegs1", "aermale1leatherlegs2", "aermale1legs1", "aermale1", "aermale1pants1", "aermale1shirt1", "aershorthair1", "aerskirt1", "aerupperarmor1", "bitingbark1", "bloodworm1", "boomerang1", "devorahair1", "dragon1", "krakenchinadress1", "lizardman1", "lizardman2", "panties1", "panties2", "plaguerat1", "seirei1", "smallrat1", "stoneimp1", "stoneimp2", "trogfem1", "trogfemarmnude1", "trogfemhead1", "trogfemlegnude1", "trogfemlonghair1", "trogfemlowernude1", "trogfemrags1", "trogfemshorthair1", "trogfemuppernude1", "wyrmhead1", "wyrmlowernude1"}) do
+for k,v in pairs({"bitingbark1", "bloodworm1", "boomerang1", "devorahair1", "dragon1", "lizardman1", "lizardman2"}) do
 	ModelSpec{name = v, edit_materials = {
 		{match_shader = "default", material = "animdiff1"},
 		{match_shader = "normalmap", material = "animskin1"},
