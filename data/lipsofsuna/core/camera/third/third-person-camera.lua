631fea50b3ecffd1bddcb571637fee15676c9568
Ari Mustonen <amuzen@users.sourceforge.net>
2013-11-03 05:11:45 +0200

    Better camera-terrain collision response using sphere casting now since it works with terrain.

--- data/lipsofsuna/core/camera/third/third-person-camera.lua
+++ data/lipsofsuna/core/camera/third/third-person-camera.lua
@@ -211,8 +211,7 @@ ThirdPersonCamera.update = function(self, secs)
 	-- Calculate the final transformation.
 	local pos,rot = self:get_eye_transform()
 	local dist = self:calculate_3rd_person_clipped_distance(pos, rot,
-		zoom, nil, self:get_collision_group(), self:get_collision_mask())
-	dist = math.max(dist - 0.2, 0)
+		zoom, 0.2, self:get_collision_group(), self:get_collision_mask())
 	pos,rot = self:calculate_3rd_person_transform(pos, rot, dist)
 	-- Mix in the camera quake.
 	if self.__quake_vector then
