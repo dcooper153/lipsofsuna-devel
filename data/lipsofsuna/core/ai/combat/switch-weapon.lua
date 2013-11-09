2762f4dd60f39c6f2c8b47ac158ff7c8494e652b
Ari Mustonen <amuzen@users.sourceforge.net>
2013-11-10 01:54:03 +0200

    Fixed broken weapon switching logic of the AI.

--- data/lipsofsuna/core/ai/combat/switch-weapon.lua
+++ data/lipsofsuna/core/ai/combat/switch-weapon.lua
@@ -4,25 +4,27 @@ Aiactionspec{
 	calculate = function(self, args)
 		-- Check that the actor can switch weapons.
 		if not args.spec.ai_enable_weapon_switch then return end
-		-- Calculate the current and new weapon ratings.
-		local curr,melee,ranged,throw = 1,1,0,0
-		if weapon then curr,melee,ranged,throw = self:calculate_weapon_ratings(weapon) end
+		-- Calculate the current weapon rating.
+		local curr = 1
+		local weapon = self.object:get_weapon()
+		if weapon then
+			curr = self:calculate_weapon_ratings(weapon)
+		end
+		-- Calculate the best weapon rating.
+		local best = 0
+		for k,item in pairs(self.object.inventory.stored) do
+			local value = self:calculate_weapon_ratings(item)
+			if value >= best then
+				best = value
+				args.best_weapon = item
+			end
+		end
 		-- Allow switching if the current weapon is worse than others.
-		local best = math.max(self.melee_rating, self.ranged_rating, self.throw_rating)
+		if args.best_weapon == weapon then return end
 		if (curr + 5 < best) or (curr == 0 and best > 0) then return 3 end
 	end,
 	perform = function(self, args)
 		local slot = self.object.spec.weapon_slot
-		if self.melee_rating > self.ranged_rating and self.melee_rating > self.throw_rating then
-			if self.best_melee_weapon then
-				self.object.inventory:equip_object(self.best_melee_weapon, slot)
-			else
-				self.object.inventory:unequip_slot(slot)
-			end
-		elseif self.ranged_rating > self.throw_rating then
-			self.object.inventory:equip_object(self.best_ranged_weapon, slot)
-		else
-			self.object.inventory:equip_object(self.best_throw_weapon, slot)
-		end
+		self.object.inventory:equip_object(args.best_weapon, slot)
 		self.action_timer = 1
 	end}
